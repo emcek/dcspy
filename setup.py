e8fc89fea8d67ea0d32af71aac8cd695b8a21fc0
Michal Plichta <michal.plichta@nokia.com>
2019-12-31 02:21:52 +0100

    tune setup.py - not finish

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-"""Various tools to emerge and to show status for conky."""
 import io
 from os.path import abspath, dirname, join
 
@@ -15,66 +14,31 @@ with io.open(join(here, 'requirements.txt'), encoding='utf-8') as f:
     requires = f.read().splitlines()
 
 setup(
-    name='pyerge',  # Required
+    name='specelUFC',  # Required
     version=__version__,  # Required
-    description='Various tools to emerge and to show status for conky',  # Required
+    description='Software for integrating DCS: F/A-18C and DCS: F-16C and Logitech G13',  # Required
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
-    url='https://github.com/emcek/pyerge',  # Optional
+    url='https://github.com/emcek/specelUFC',  # Optional
     author='Michal Plichta',  # Optional
     license='GPLv2',
     scripts=['script/pye'],
-    # entry_points={'console_scripts': ['exec = pyerge.cli:run_parser']},
-    classifiers=[  # Optional
-        'Development Status :: 4 - Beta',
-
-        'Environment :: Console',
-        'Environment :: X11 Applications',
-
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-
-        'Operating System :: POSIX',
-        'Operating System :: Unix',
-
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-
-        'Topic :: Desktop Environment',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Monitoring',
-        'Topic :: Utilities'
-    ],
-
-    keywords='gentoo portage emerge conky',  # Optional
-
-    # You can just specify package directories manually here if your project is
-    # simple. Or you can use find_packages().
-    #
-    # Alternatively, if you just want to distribute a single Python file, use
-    # the `py_modules` argument instead as follows, which will expect a file
-    # called `my_module.py` to exist:
-    #
-    #   py_modules=["my_module"],
-    #
+    classifiers=['Development Status :: 4 - Beta',
+                 'Environment :: Console',
+                 'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: Microsoft :: Windows :: Windows 10',
+                 'Topic :: Games/Entertainment',
+                 'Topic :: Games/Entertainment :: Simulation',
+                 'Topic :: Software Development :: Libraries :: Python Modules',
+                 'Topic :: System :: Hardware',
+                 'Topic :: Utilities'],
+    keywords='dcs g13 logitech',  # Optional
     packages=find_packages(exclude=['tests']),  # Required
-
-    # This field lists other packages that your project depends on to run.
-    # Any package you put here will be installed by pip when your project is
-    # installed, so they must be valid existing projects.
-    #
-    # For an analysis of "install_requires" vs pip's requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
     install_requires=requires,  # Optional
-
-    extras_require={
-        'testing': ['pytest']
-    },
-
-    project_urls={  # Optional
-        'Bug Reports': 'https://github.com/emcek/pyerge/issues',
-        'Source': 'https://github.com/emcek/pyerge',
-    },
-)
+    extras_require={'testing': ['pytest']},
+    project_urls={'Bug Reports': 'https://github.com/emcek/specelUFC/issues',
+                  'Source': 'https://github.com/emcek/specelUFC'})
