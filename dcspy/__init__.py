ce682b5d9dbf1786f2b834c08c7ae6f27854f229
Michal Plichta <michal.plichta@nokia.com>
2020-01-07 22:56:46 +0100

    temp. workaround for fonts

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -2,5 +2,12 @@ from PIL import ImageFont
 
 __version__ = '0.9.1'
 SUPPORTED_CRAFTS = ('FA-18C_hornet', 'Ka-50', 'F-16C_50')
-FONT1 = ImageFont.truetype('consola.ttf', 11)
-FONT2 = ImageFont.truetype('consola.ttf', 16)
+# @todo: find better way
+try:
+    FONT1 = ImageFont.truetype('consola.ttf', 11)
+except OSError:
+    FONT1 = ImageFont.truetype('DejaVuSansMono.ttf', 11)
+try:
+    FONT2 = ImageFont.truetype('consola.ttf', 16)
+except OSError:
+    FONT2 = ImageFont.truetype('DejaVuSansMono.ttf', 16)
