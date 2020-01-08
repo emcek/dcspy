71ea7c6992549aee3bdeac6bd6e8e8e3f6b29d15
Michal Plichta <michal.plichta@nokia.com>
2020-01-09 00:29:39 +0100

    add one test for hornet

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -1,10 +1,24 @@
 from pytest import mark
 
+width = 160
+height = 43
+
 
 @mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50'])
 def test_check_all_aircraft_inherit_from_correct_base_class(model):
     from dcspy import aircrafts
     aircraft = getattr(aircrafts, model)
-    aircraft_model = aircraft(160, 43)
+    aircraft_model = aircraft(width, height)
     assert isinstance(aircraft_model, aircrafts.Aircraft)
     assert issubclass(aircraft, aircrafts.Aircraft)
+
+
+@mark.parametrize('button, result', [(0, '\n'),
+                                     (5, '\n'),
+                                     ('a', '\n'),
+                                     (1, 'UFC_COMM1_CHANNEL_SELECT DEC\n'),
+                                     (4, 'UFC_COMM2_CHANNEL_SELECT INC\n')])
+def test_button_pressed_for_hornet(button, result):
+    from dcspy import aircrafts
+    aircraft = aircrafts.FA18Chornet(width, height)
+    assert aircraft.button_handle_specific_ac(button) == result
