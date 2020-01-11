c63341cf09cb04d481bd738a806e11411e86ac48
Michal Plichta <michal.plichta@nokia.com>
2020-01-11 16:06:54 +0100

    add extra system info when starting

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -1,4 +1,6 @@
-from logging import basicConfig, DEBUG
+from logging import basicConfig, DEBUG, debug
+from os import name
+from platform import architecture, uname, python_implementation, python_version
 from sys import platform
 
 from PIL import ImageFont
@@ -6,6 +8,11 @@ from PIL import ImageFont
 basicConfig(format='%(asctime)s | %(levelname)-7s | %(message)s / %(filename)s:%(lineno)d', level=DEBUG)
 __version__ = '0.9.1'
 SUPPORTED_CRAFTS = ('FA-18C_hornet', 'Ka-50', 'F-16C_50')
+
+debug(f'Arch: {name} / {platform} / {" / ".join(architecture())}')
+debug(f'Python: {python_implementation()}-{python_version()}')
+debug(f'{uname()}')
+
 
 if platform == 'win32':
     FONT_11 = ImageFont.truetype('consola.ttf', 11)
