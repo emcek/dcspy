614a41130b3322beb68c0ed04fa4c2217a7cd781
Michal Plichta <michal.plichta@nokia.com>
2020-01-15 12:44:05 +0100

    add new test form addr_low to addr_high

--- tests/test_dcsbios.py
+++ tests/test_dcsbios.py
@@ -5,3 +5,13 @@ def test_process_byte_wait_for_sync_to_address_low():
     for _ in range(4):
         p.process_byte(bytes([0x55]))
     assert p.state == 'ADDRESS_LOW'
+
+
+def test_process_byte_address_low_to_address_high():
+    from dcspy.dcsbios import ProtocolParser
+    p = ProtocolParser()
+    p.state = 'ADDRESS_LOW'
+    data_sent = 0x0f
+    p.process_byte(bytes([data_sent]))
+    assert p.state == 'ADDRESS_HIGH'
+    assert p.address == data_sent
