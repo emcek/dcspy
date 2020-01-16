f271c3c7e3b88a14959f0a61ce5e251569046239
Michal Plichta <michal.plichta@nokia.com>
2020-01-16 15:06:45 +0100

    add fixture for w and h od LCD for aircraft module

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,22 @@
 from pytest import fixture
 
 
+# <=><=><=><=><=> dcsbios <=><=><=><=><=>
 @fixture
 def protocol_parser():
-    """Empty instance of ProtocolParser"""
+    """Basic instance of ProtocolParser"""
     from dcspy.dcsbios import ProtocolParser
     return ProtocolParser()
+
+
+# <=><=><=><=><=> aircrafts <=><=><=><=><=>
+@fixture()
+def lcd_size() -> Tuple[int, int]:
+    """
+    Return width and height of G13 LCD as tuple of integers.
+
+    :return: width and height
+    :rtype: Tuple[int, int]
+    """
+    from dcspy.sdk.lcd_sdk import MONO_WIDTH, MONO_HEIGHT
+    return MONO_WIDTH, MONO_HEIGHT
--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -2,24 +2,21 @@ from unittest import mock
 
 from pytest import mark, raises
 
-width = 160
-height = 43
-
 
 @mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50'])
-def test_check_all_aircraft_inherit_from_correct_base_class(model):
+def test_check_all_aircraft_inherit_from_correct_base_class(model, lcd_size):
     from dcspy import aircrafts
     aircraft = getattr(aircrafts, model)
-    aircraft_model = aircraft(width, height)
+    aircraft_model = aircraft(*lcd_size)
     assert isinstance(aircraft_model, aircrafts.Aircraft)
     assert issubclass(aircraft, aircrafts.Aircraft)
 
 
-def test_aircraft_base_class():
+def test_aircraft_base_class(lcd_size):
     from dcspy import aircrafts
     from PIL import Image
     with mock.patch.object(aircrafts, 'lcd_sdk', return_value=None) as lcd_sdk_mock:
-        aircraft = aircrafts.Aircraft(width, height)
+        aircraft = aircrafts.Aircraft(*lcd_size)
         aircraft.bios_data = {'abstract_field': {'addr': 0xdeadbeef, 'len': 16, 'val': ''}}
 
         assert aircraft.button_request(1) == '\n'
@@ -32,7 +29,7 @@ def test_aircraft_base_class():
 
         assert aircraft.get_bios('abstract_field') == 'deadbeef'
         assert aircraft.get_bios('none') == ''
-        img = Image.new('1', (width, height), 0)
+        img = Image.new('1', lcd_size, 0)
         assert aircraft.update_display(img) is None
         lcd_sdk_mock.update_display.assert_called_once_with(img)
 
@@ -42,33 +39,33 @@ def test_aircraft_base_class():
                                      ('a', '\n'),
                                      (1, 'UFC_COMM1_CHANNEL_SELECT DEC\n'),
                                      (4, 'UFC_COMM2_CHANNEL_SELECT INC\n')])
-def test_button_pressed_for_hornet(button, result):
+def test_button_pressed_for_hornet(button, result, lcd_size):
     from dcspy.aircrafts import FA18Chornet
-    aircraft = FA18Chornet(width, height)
+    aircraft = FA18Chornet(*lcd_size)
     assert aircraft.button_request(button) == result
 
 
 @mark.parametrize('selector, value, result', [('ScratchpadStr2', '~~', '22'),
                                               ('COMM1', '``', '11'),
                                               ('FuelTotal', '104T', '104T')])
-def test_set_bios_for_hornet(selector, value, result):
+def test_set_bios_for_hornet(selector, value, result, lcd_size):
     from dcspy.aircrafts import FA18Chornet
-    aircraft = FA18Chornet(width, height)
+    aircraft = FA18Chornet(*lcd_size)
     aircraft.set_bios(selector, value, False)
     assert aircraft.bios_data[selector]['val'] == result
 
 
 @mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50'])
-def test_prepare_image_for_all_palnes(model):
+def test_prepare_image_for_all_palnes(model, lcd_size):
     from PIL.Image import Image
     from dcspy import aircrafts
     aircraft = getattr(aircrafts, model)
-    aircraft_model = aircraft(width, height)
+    aircraft_model = aircraft(*lcd_size)
     if model == 'Ka50':
         aircraft_model.set_bios('l1_text', '123456789', False)
         aircraft_model.set_bios('l2_text', '987654321', False)
     img = aircraft_model.prepare_image()
-    assert img.size == (width, height)
+    assert img.size == lcd_size
     assert isinstance(img, Image)
 
 
@@ -77,7 +74,7 @@ def test_prepare_image_for_all_palnes(model):
                                      ('a', '\n'),
                                      (2, 'PVI_FIXPOINTS_BTN 1\nPVI_FIXPOINTS_BTN 0\n'),
                                      (3, 'PVI_AIRFIELDS_BTN 1\nPVI_AIRFIELDS_BTN 0\n')])
-def test_button_pressed_for_shark(button, result):
+def test_button_pressed_for_shark(button, result, lcd_size):
     from dcspy.aircrafts import Ka50
-    aircraft = Ka50(width, height)
+    aircraft = Ka50(*lcd_size)
     assert aircraft.button_request(button) == result
