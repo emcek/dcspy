92ce5821d925868ade9d9a15e1ad44debe9a625a
Michal Plichta <michal.plichta@nokia.com>
2020-02-03 20:33:06 +0100

    add _sock_err_handler and move Ctrl_C handler upper

--- dcspy/dcspy.py
+++ dcspy/dcspy.py
@@ -55,24 +55,33 @@ def _handle_connection(g13: G13, parser: ProtocolParser, sock: socket.socket) ->
     :param sock: multi-cast UDP socket
     :type sock: socket.socket
     """
+    start = time()
     while True:
         try:
             dcs_bios_resp = sock.recv(2048)
             for int_byte in dcs_bios_resp:
                 parser.process_byte(int_byte)
+                start = time()
             if g13.plane_detected:
                 g13.load_new_plane()
             g13.button_handle(sock)
         except socket.error as exp:
+            _sock_err_handler(g13, start)
             debug(f'Main loop socket error: {exp}')
             info('Waiting for DCS connection...')
-        except KeyboardInterrupt:
-            info('Exit due to Ctrl-C')
-            sys.exit(0)
+
+
+def _sock_err_handler(g13, start):
+    wait_time = gmtime(time() - start)
+    spacer = ' ' * 13
+    g13.display = ['Logitech G13 OK', 'No new data from DCS:',
+                   f'{spacer}{wait_time.tm_min:02d}:{wait_time.tm_sec:02d} [min:s]', f'dcspy: v{__version__}']
 
 
 def run():
     """Main of running function."""
+    info(f'dcspy {__version__} https://github.com/emcek/dcspy')
+    check_current_version()
     parser = ProtocolParser()
     g13 = G13(parser)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
@@ -80,7 +89,12 @@ def run():
     sock.bind(RECV_ADDR)
     mreq = struct.pack('=4sl', socket.inet_aton(MULTICAST_IP), socket.INADDR_ANY)
     sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
-    _handle_connection(g13, parser, sock)
+    sock.settimeout(1)
+    try:
+        _handle_connection(g13, parser, sock)
+    except KeyboardInterrupt:
+        info('Exit due to Ctrl-C')
+        sys.exit(0)
 
 
 def run1() -> None:
