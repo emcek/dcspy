99a6771159a0124f48ed056004a971c7df4b05d6
Michal Plichta <michal.plichta@nokia.com>
2020-02-27 22:32:47 +0100

    test update - no joy

--- tests/test_dcsbios.py
+++ tests/test_dcsbios.py
@@ -135,9 +135,9 @@ def test_string_buffer_callback(protocol_parser):
         assert args == (1,)
         assert kwargs == dict()
 
-    StringBuffer(parser=protocol_parser, address=0x748a, length=6, callback=partial(_callback))
+    StringBuffer(parser=protocol_parser, address=0x1930, length=1, callback=partial(_callback))
     protocol_parser.state = 'DATA_HIGH'
     protocol_parser.count = 1
-    protocol_parser.address = 0x748a
-    protocol_parser.data = 0x666
-    protocol_parser.process_byte(0x11)
+    protocol_parser.data = 0x31
+    protocol_parser.address = 0x1930
+    protocol_parser.process_byte(0x0)
