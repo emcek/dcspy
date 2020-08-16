0a46b92a9623b69567349886d75da90c27e2e93f
Michal Plichta <michal.plichta@nokia.com>
2020-08-16 23:50:11 +0200

    extract gui to separate module

--- dcspy/dcspy.py
+++ dcspy/dcspy.py
@@ -2,14 +2,17 @@ import sys
 import tkinter as tk
 from logging import getLogger
 
+from dcspy.tk_gui import DcspyGui
+
 LOG = getLogger(__name__)
 
 
 def run():
     """Function to start DCSpy GUI."""
     root = tk.Tk()
-    root.geometry('150x80')
-    root.minsize(150, 80)
+    w, h = 150, 100
+    root.geometry(f'{w}x{h}')
+    root.minsize(w, h)
     root.iconbitmap(f'{sys.prefix}/dcspy_data/dcspy.ico')
     gui = DcspyGui(master=root)
     gui.mainloop()
---
+++ dcspy/tk_gui.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from logging import getLogger
+
+LOG = getLogger(__name__)
+
+
+class DcspyGui(tk.Frame):
+    def __init__(self, master: tk.Tk) -> None:
+        """
+        Basic constructor.
+
+        :param master: Top level widget
+        """
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.lcd_type = tk.StringVar()
+        self._init_widgets()
+
+    def _init_widgets(self) -> None:
+        frame = tk.Frame(master=self.master, relief=tk.GROOVE, borderwidth=2)
+        lcd_types = {'G19': 'G19', 'G510': 'G510', 'G15 v1/v2': 'G15', 'G13': 'G13'}
+        for text, value in lcd_types.items():
+            rb = tk.Radiobutton(master=frame, text=text, variable=self.lcd_type, value=value, command=self._lcd_type_selected)
+            rb.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
+            rb.select()
+
+        start = tk.Button(master=self.master, text='Start', command=DcspyGui.start_dcspy)
+        close = tk.Button(master=self.master, text='Close', command=self.master.destroy)
+
+        frame.pack(side=tk.LEFT, fill=tk.Y, expand=True)
+        start.pack(side=tk.TOP, fill=tk.X, expand=True)
+        close.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
+
+    def _lcd_type_selected(self) -> None:
+        """Handling selected LCD type"""
+        LOG.info(f'Logitech {self.lcd_type} selected')
+
+    @staticmethod
+    def start_dcspy() -> None:
+        from dcspy import starter
+        starter.run()
