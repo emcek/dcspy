404cc5034ae498844817ca53b8193c80bcd8005a
Michal Plichta <michal.plichta@nokia.com>
2020-08-22 22:48:46 +0200

    der init

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -14,6 +14,7 @@ SEND_ADDR = ('127.0.0.1', 7778)
 RECV_ADDR = ('', 5010)
 MULTICAST_IP = '239.255.50.10'
 LcdSize = NamedTuple('lcd_size', [('width', int), ('height', int)])
+LCD_TYPES = {'G19': 'KeyboardColor', 'G510': 'KeyboardMono', 'G15 v1/v2': 'KeyboardMono', 'G13': 'KeyboardMono'}
 LOG = getLogger(__name__)
 config_logger(LOG)
 
