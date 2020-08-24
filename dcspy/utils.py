680ae935d9c537ff423efcf5f9bc63cbc052b935
Michal Plichta <michal.plichta@nokia.com>
2020-08-25 00:47:21 +0200

    workaround for tests

--- dcspy/utils.py
+++ dcspy/utils.py
@@ -1,8 +1,13 @@
 from os import path
-from sys import prefix
+from sys import platform, prefix
 
 from PIL import ImageFont
 
-DEDFONT_11 = ImageFont.truetype(path.join(prefix, 'dcspy_data', 'falconded.ttf'), 11)
-FONT_11 = ImageFont.truetype('consola.ttf', 11)
-FONT_16 = ImageFont.truetype('consola.ttf', 16)
+
+if platform == 'win32':
+    DEDFONT_11 = ImageFont.truetype(path.join(prefix, 'dcspy_data', 'falconded.ttf'), 11)
+    FONT_11 = ImageFont.truetype('consola.ttf', 11)
+    FONT_16 = ImageFont.truetype('consola.ttf', 16)
+else:
+    FONT_11 = ImageFont.truetype('DejaVuSansMono.ttf', 11)
+    FONT_16 = ImageFont.truetype('DejaVuSansMono.ttf', 16)
