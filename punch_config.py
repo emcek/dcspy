d4eb4f067893da22ffdf6fa5b43686b5c8869571
Michal Plichta <michal.plichta@nokia.com>
2020-10-19 21:40:36 +0200

    add punch configuration

---
+++ punch_config.py
@@ -0,0 +1,18 @@
+__config_version__ = 1
+
+GLOBALS = {
+    'serializer': '{{major}}.{{minor}}.{{patch}}',
+}
+
+FILES = ['setup.py',
+         'dcspy/dcspy.py',
+         'dcspy/starter.py']
+
+VERSION = ['major', 'minor', 'patch']
+
+# VCS = {
+#     'name': 'git',
+#     'commit_message': (
+#         "Version updated from {{ current_version }}"
+#         " to {{ new_version }}")
+# }
