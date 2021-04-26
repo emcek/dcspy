9ab71097d2a759ebde2b0cc15c8881cbcf20b18c
Michal Plichta <michal.plichta@nokia.com>
2021-04-26 22:05:45 +0200

    fix test for py36

--- tests/test_starter.py
+++ tests/test_starter.py
@@ -31,5 +31,5 @@ def test_prepare_socket():
     assert isinstance(sock, socket.socket)
     assert sock.proto == 17
     assert sock.gettimeout() == 1.0
-    assert sock.type == 2
+    assert sock.type in (2050, 2)
     assert sock.family == 2
