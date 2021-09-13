138eedca0cd3b00dcee2a71a00e2c0d66dd78172
Michal Plichta <michal.plichta@nokia.com>
2021-09-13 19:14:10 +0200

    pass bios_data not whole plane instance

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -16,7 +16,7 @@ else:
 
 def test_bios_values_for_shark(black_shark_mono):
     local_json = _get_json_for_plane('Ka-50.json')
-    results = _check_dcsbios_data(black_shark_mono, local_json)
+    results = _check_dcsbios_data(black_shark_mono.bios_data, local_json)
     print('\nShark BIOS Report\n-----------------')
     pprint(results)
     # assert not results
@@ -24,7 +24,7 @@ def test_bios_values_for_shark(black_shark_mono):
 
 def test_bios_values_for_viper(viper_mono):
     local_json = _get_json_for_plane('F-16C_50.json')
-    results = _check_dcsbios_data(viper_mono, local_json)
+    results = _check_dcsbios_data(viper_mono.bios_data, local_json)
     print('\nViper BIOS Report\n-----------------')
     pprint(results)
     # assert not results
@@ -32,7 +32,7 @@ def test_bios_values_for_viper(viper_mono):
 
 def test_bios_values_for_hornet(hornet_mono):
     local_json = _get_json_for_plane('FA-18C_hornet.json')
-    results = _check_dcsbios_data(hornet_mono, local_json)
+    results = _check_dcsbios_data(hornet_mono.bios_data, local_json)
     print('\nHornet BIOS Report\n------------------')
     pprint(results)
     # assert not results
@@ -40,7 +40,7 @@ def test_bios_values_for_hornet(hornet_mono):
 
 def test_bios_values_for_tomcat(tomcat_mono):
     local_json = _get_json_for_plane('F-14B.json')
-    results = _check_dcsbios_data(tomcat_mono, local_json)
+    results = _check_dcsbios_data(tomcat_mono.bios_data, local_json)
     print('\nTomcat BIOS Report\n------------------')
     pprint(results)
     # assert not results
@@ -51,16 +51,16 @@ def _get_json_for_plane(plane: str) -> dict:
     return loads(data.content)
 
 
-def _check_dcsbios_data(plane_bios, local_json) -> dict:
+def _check_dcsbios_data(plane_bios: dict, local_json) -> dict:
     results = {}
-    for bios_key in plane_bios.bios_data:
+    for bios_key in plane_bios:
         bios_ref = _recursive_lookup(bios_key, local_json)
         if not bios_ref:
             results[bios_key] = f'Not found in DCS-BIOS {dcsbios_ver}'
             continue
         bios_outputs = bios_ref['outputs'][0]
-        for args_key in plane_bios.bios_data[bios_key]['args']:
-            aircraft_value = plane_bios.bios_data[bios_key]['args'][args_key]
+        for args_key in plane_bios[bios_key]['args']:
+            aircraft_value = plane_bios[bios_key]['args'][args_key]
             dcsbios_value = bios_outputs[check_convert[args_key]]
             if not aircraft_value == dcsbios_value:
                 bios_issue = {args_key: f"dcspy: {aircraft_value} ({hex(aircraft_value)}) "
