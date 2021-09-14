93617d4e07ed5200a7dbd1f83208f7bc96c79aa3
Michal Plichta <michal.plichta@nokia.com>
2021-09-14 12:33:24 +0200

    extract name from class

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -1,10 +1,11 @@
 from pprint import pprint
 
+from dcspy import SUPPORTED_CRAFTS
 from utils import check_dcsbios_data
 
 
 def test_bios_values_for_shark(black_shark_mono):
-    name = 'Ka-50'
+    name = SUPPORTED_CRAFTS[black_shark_mono.__class__.__name__]
     results = check_dcsbios_data(black_shark_mono.bios_data, f'{name}.json')
     print(f'\n{name} BIOS Report\n{"-" * (len(name) + 12)}')
     pprint(results)
@@ -12,7 +13,7 @@ def test_bios_values_for_shark(black_shark_mono):
 
 
 def test_bios_values_for_viper(viper_mono):
-    name = 'F-16C_50'
+    name = SUPPORTED_CRAFTS[viper_mono.__class__.__name__]
     results = check_dcsbios_data(viper_mono.bios_data, f'{name}.json')
     print(f'\n{name} BIOS Report\n{"-" * (len(name) + 12)}')
     pprint(results)
@@ -20,7 +21,7 @@ def test_bios_values_for_viper(viper_mono):
 
 
 def test_bios_values_for_hornet(hornet_mono):
-    name = 'FA-18C_hornet'
+    name = SUPPORTED_CRAFTS[hornet_mono.__class__.__name__]
     results = check_dcsbios_data(hornet_mono.bios_data, f'{name}.json')
     print(f'\n{name} BIOS Report\n{"-" * (len(name) + 12)}')
     pprint(results)
@@ -28,7 +29,7 @@ def test_bios_values_for_hornet(hornet_mono):
 
 
 def test_bios_values_for_tomcat(tomcat_mono):
-    name = 'F-14B'
+    name = SUPPORTED_CRAFTS[tomcat_mono.__class__.__name__]
     results = check_dcsbios_data(tomcat_mono.bios_data, f'{name}.json')
     print(f'\n{name} BIOS Report\n{"-" * (len(name) + 12)}')
     pprint(results)
