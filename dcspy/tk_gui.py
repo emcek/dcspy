ed85ebefd7f8a214d5ead732ccd088e01929bfa3
Michal Plichta <michal.plichta@nokia.com>
2021-09-18 17:52:40 +0200

    add draft for simple config editor

--- dcspy/tk_gui.py
+++ dcspy/tk_gui.py
@@ -1,10 +1,11 @@
 import tkinter as tk
 from logging import getLogger
+from sys import prefix
 from threading import Thread
 
 from dcspy import LCD_TYPES, config
-from dcspy.utils import save_cfg
 from dcspy.starter import dcspy_run
+from dcspy.utils import save_cfg
 
 LOG = getLogger(__name__)
 
@@ -21,6 +22,7 @@ class DcspyGui(tk.Frame):
         self.master.title('GUI')
         self.lcd_type = tk.StringVar()
         self.status_txt = tk.StringVar()
+        self.cfg_status = tk.StringVar()
         self._init_widgets()
 
     def _init_widgets(self) -> None:
@@ -29,6 +31,7 @@ class DcspyGui(tk.Frame):
         self.master.rowconfigure(index=0, weight=1)
         self.master.rowconfigure(index=1, weight=1)
         self.master.rowconfigure(index=2, weight=1)
+        self.master.rowconfigure(index=3, weight=1)
 
         frame = tk.Frame(master=self.master, relief=tk.GROOVE, borderwidth=2)
         for i, text in enumerate(LCD_TYPES):
@@ -37,14 +40,16 @@ class DcspyGui(tk.Frame):
             if config.get('keyboard', 'G13') == text:
                 rb_lcd_type.select()
 
-        start = tk.Button(master=self.master, text='Start', command=self.start_dcspy)
-        close = tk.Button(master=self.master, text='Close', command=self.master.destroy)
+        start = tk.Button(master=self.master, text='Start', width=6, command=self.start_dcspy)
+        cfg = tk.Button(master=self.master, text='Config', width=6, command=self._cfg_window)
+        close = tk.Button(master=self.master, text='Close', width=6, command=self.master.destroy)
         status = tk.Label(master=self.master, textvariable=self.status_txt)
 
-        frame.grid(row=0, column=0, padx=2, pady=2, rowspan=2)
+        frame.grid(row=0, column=0, padx=2, pady=2, rowspan=3)
         start.grid(row=0, column=1, padx=2, pady=2)
-        close.grid(row=1, column=1, padx=2, pady=2)
-        status.grid(row=2, column=0, columnspan=2, sticky=tk.W)
+        cfg.grid(row=1, column=1, padx=2, pady=2)
+        close.grid(row=2, column=1, padx=2, pady=2)
+        status.grid(row=3, column=0, columnspan=2, sticky=tk.W)
 
     def _lcd_type_selected(self) -> None:
         """Handling selected LCD type."""
@@ -52,6 +57,39 @@ class DcspyGui(tk.Frame):
         LOG.debug(f'Logitech {keyboard} selected')
         self.status_txt.set(f'Logitech {keyboard} selected')
         save_cfg(cfg_dict={'keyboard': keyboard})
+
+    def _cfg_window(self):
+        cfg_edit = tk.Toplevel(self.master)
+        cfg_edit.title('Config Editor')
+        width, height = 450, 200
+        cfg_edit.geometry(f'{width}x{height}')
+        cfg_edit.minsize(width=250, height=150)
+        cfg_edit.iconbitmap(f'{prefix}/dcspy_data/dcspy.ico')
+
+        scrollbar_y = tk.Scrollbar(cfg_edit, orient='vertical')
+        scrollbar_y.pack(side=tk.RIGHT, fill=tk.Y)
+        # from tkinter import font
+        # print(font.families())
+        # print(font.names())
+        text_info = tk.Text(master=cfg_edit, width=10, height=5, yscrollcommand=scrollbar_y.set, wrap=tk.WORD,
+                            font=('Courier New', 10), selectbackground='purple', selectforeground='white', undo=True)
+        text_info.pack(fill=tk.BOTH, expand=True)
+        scrollbar_y.config(command=text_info.yview)
+        load = tk.Button(master=cfg_edit, text='Load', width=6, command=self._load_cfg)
+        save = tk.Button(master=cfg_edit, text='Save', width=6, command=self._save_cfg)
+        quit = tk.Button(master=cfg_edit, text='Quit', width=6, command=cfg_edit.destroy)
+        load.pack(side=tk.LEFT)
+        save.pack(side=tk.LEFT)
+        quit.pack(side=tk.LEFT)
+        statusbar = tk.Label(master=cfg_edit, textvariable=self.cfg_status, anchor=tk.E)
+        self.cfg_status.set('Ready')
+        statusbar.pack(side=tk.BOTTOM, fill=tk.X)
+
+    def _load_cfg(self):
+        pass
+
+    def _save_cfg(self):
+        pass
 
     def start_dcspy(self) -> None:
         """Run real application."""
