1684ce340b9974e419d4033d9f51299c79aaf51d
Michal Plichta <michal.plichta@nokia.com>
2021-09-19 02:23:02 +0200

    fix test for set_defaults

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 def test_dummy_save_load_set_defaults():
     from dcspy import utils
-    from os import makedirs, remove, rmdir
+    from os import makedirs, remove, rmdir, environ
     makedirs(name='./tmp', exist_ok=True)
     test_tmp_yaml = './tmp/c.yaml'
 
@@ -8,7 +8,7 @@ def test_dummy_save_load_set_defaults():
     d_cfg = utils.load_cfg(test_tmp_yaml)
     assert d_cfg == {'1': 1}
     d_cfg = utils.set_defaults(d_cfg)
-    assert d_cfg == {'keyboard': 'G13', 'dcsbios': 'D:\\Users\\UNKNOWN\\Saved Games\\DCS.openbeta\\Scripts\\DCS-BIOS',
+    assert d_cfg == {'keyboard': 'G13', 'dcsbios': f'D:\\Users\\{environ.get("USERNAME", "UNKNOWN")}\\Saved Games\\DCS.openbeta\\Scripts\\DCS-BIOS',
                      'fontname': 'consola.ttf', 'fontsize': [11, 16, 22, 32]}
     with open(test_tmp_yaml, 'w+') as f:
         f.write('')
