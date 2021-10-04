bfd8337187d2e541addaac223e8e6bf03b5acea2
Michal Plichta <michal.plichta@nokia.com>
2021-10-04 19:07:56 +0200

    add fg and bg to LcdSize

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -2,7 +2,7 @@ from logging import getLogger
 from os import name
 from platform import architecture, uname, python_implementation, python_version
 from sys import platform
-from typing import NamedTuple
+from typing import NamedTuple, Union, Sequence
 
 from PIL import ImageFont
 
@@ -14,9 +14,10 @@ SUPPORTED_CRAFTS = {'FA18Chornet': 'FA-18C_hornet', 'Ka50': 'Ka-50', 'F16C50': '
 SEND_ADDR = ('127.0.0.1', 7778)
 RECV_ADDR = ('', 5010)
 MULTICAST_IP = '239.255.50.10'
-LcdSize = NamedTuple('LcdSize', [('width', int), ('height', int), ('type', int)])
-LcdMono = LcdSize(width=lcd_sdk.MONO_WIDTH, height=lcd_sdk.MONO_HEIGHT, type=lcd_sdk.TYPE_MONO)
-LcdColor = LcdSize(width=lcd_sdk.COLOR_WIDTH, height=lcd_sdk.COLOR_HEIGHT, type=lcd_sdk.TYPE_COLOR)
+LcdSize = NamedTuple('LcdSize', [('width', int), ('height', int), ('type', int),
+                                 ('fg', Union[int, Sequence[int]]), ('bg', Union[int, Sequence[int]])])
+LcdMono = LcdSize(width=lcd_sdk.MONO_WIDTH, height=lcd_sdk.MONO_HEIGHT, type=lcd_sdk.TYPE_MONO, fg=255, bg=0)
+LcdColor = LcdSize(width=lcd_sdk.COLOR_WIDTH, height=lcd_sdk.COLOR_HEIGHT, type=lcd_sdk.TYPE_COLOR, fg=(0, 255, 0, 255), bg=(0, 0, 0, 0))
 LCD_TYPES = {'G19': 'KeyboardColor', 'G510': 'KeyboardMono', 'G15 v1/v2': 'KeyboardMono', 'G13': 'KeyboardMono'}
 LOG = getLogger(__name__)
 config_logger(LOG)
