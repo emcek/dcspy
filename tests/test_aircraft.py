f72f47ea62b6840aa44c693949360347f277daa5
Michal Plichta <michal.plichta@nokia.com>
2021-10-15 00:13:08 +0200

    add few tests for harrier

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -71,6 +71,24 @@ def test_button_pressed_for_hornet_color(button, result, hornet_color):
     assert hornet_color.button_request(button) == result
 
 
+@mark.parametrize('button, result', [(0, '\n'),
+                                     (16, '\n'),
+                                     ('g', '\n'),
+                                     (2, 'UFC_COM1_SEL 3200\n'),
+                                     (3, 'UFC_COM2_SEL -3200\n')])
+def test_button_pressed_for_harierr_mono(button, result, harrier_mono):
+    assert harrier_mono.button_request(button) == result
+
+
+@mark.parametrize('button, result', [(0, '\n'),
+                                     (16, '\n'),
+                                     ('.', '\n'),
+                                     (10, 'UFC_COM1_SEL 3200\n'),
+                                     (13, 'UFC_COM2_SEL 3200\n')])
+def test_button_pressed_for_harrier_color(button, result, harrier_color):
+    assert harrier_color.button_request(button) == result
+
+
 @mark.parametrize('selector, value, result', [('UFC_SCRATCHPAD_STRING_2_DISPLAY', '~~', '22'),
                                               ('UFC_COMM1_DISPLAY', '``', '11'),
                                               ('IFEI_FUEL_UP', '104T', '104T')])
@@ -137,7 +155,7 @@ def test_get_next_value_for_button_in_viper(viper_color):
     assert isinstance(viper_color.cycle_buttons[name9], cycle)
 
 
-@mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50', 'A10C', 'A10C2', 'F14B'])
+@mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50', 'A10C', 'A10C2', 'F14B', 'AV8BNA'])
 def test_prepare_image_for_all_palnes_mono(model, lcd_mono):
     from PIL.Image import Image
     from dcspy import aircrafts
@@ -156,7 +174,7 @@ def test_prepare_image_for_all_palnes_mono(model, lcd_mono):
     assert img.mode == '1'
 
 
-@mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50', 'A10C', 'A10C2', 'F14B'])
+@mark.parametrize('model', ['FA18Chornet', 'F16C50', 'Ka50', 'A10C', 'A10C2', 'F14B', 'AV8BNA'])
 def test_prepare_image_for_all_palnes_color(model, lcd_color):
     from PIL.Image import Image
     from dcspy import aircrafts
