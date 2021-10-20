a03c90f148f4f75a9292285beb4506cc5a73677c
Michal Plichta <michal.plichta@nokia.com>
2021-10-20 02:47:11 +0200

    add simple possitive test for LED SDK

--- tests/test_led_sdk.py
+++ tests/test_led_sdk.py
@@ -17,3 +17,21 @@ def test_all_failure_cases(function, args, result):
     from dcspy.sdk import led_sdk
     led_sdk.LED_DLL = None
     assert getattr(led_sdk, function)(*args) is result
+
+
+@mark.parametrize('py_func, c_func, args, result', [('logi_led_init', 'LogiLedInit', (), True),
+                                                    ('logi_led_init_with_name', 'LogiLedInitWithName', ('name',), True),
+                                                    ('logi_led_set_target_device', 'LogiLedSetTargetDevice', (1,), True),
+                                                    ('logi_led_save_current_lighting', 'LogiLedSaveCurrentLighting', (), True),
+                                                    ('logi_led_restore_lighting', 'LogiLedRestoreLighting', (), True),
+                                                    ('logi_led_set_lighting', 'LogiLedSetLighting', ((1, 2, 3),), True),
+                                                    ('logi_led_flash_lighting', 'LogiLedFlashLighting', ((1, 2, 3), 1, 1), True),
+                                                    ('logi_led_pulse_lighting', 'LogiLedPulseLighting', ((1, 2, 3), 1, 1), True),
+                                                    ('logi_led_stop_effects', 'LogiLedStopEffects', (), True),
+                                                    ('logi_led_shutdown', 'LogiLedShutdown', (), True)])
+def test_all_success_cases(py_func, c_func, args, result):
+    from dcspy.sdk import led_sdk
+    mocked_c_func = Mock()
+    mocked_c_func.return_value = result
+    led_sdk.LED_DLL = {c_func: mocked_c_func}
+    assert getattr(led_sdk, py_func)(*args) is result
