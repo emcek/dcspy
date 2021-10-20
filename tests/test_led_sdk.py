95618e2a56ae5b99e9f7561169c8f63bdbc3c73b
Michal Plichta <michal.plichta@nokia.com>
2021-10-20 02:39:39 +0200

    add simple negative test for LED SDK

---
+++ tests/test_led_sdk.py
@@ -0,0 +1,19 @@
+from unittest.mock import Mock
+
+from pytest import mark
+
+
+@mark.parametrize('function, args, result', [('logi_led_init', (), False),
+                                             ('logi_led_init_with_name', ('name',), False),
+                                             ('logi_led_set_target_device', (1,), False),
+                                             ('logi_led_save_current_lighting', (), False),
+                                             ('logi_led_restore_lighting', (), False),
+                                             ('logi_led_set_lighting', ((1, 2, 3),), False),
+                                             ('logi_led_flash_lighting', ((1, 2, 3), 1, 1), False),
+                                             ('logi_led_pulse_lighting', ((1, 2, 3), 1, 1), False),
+                                             ('logi_led_stop_effects', (), False),
+                                             ('logi_led_shutdown', (), False)])
+def test_all_failure_cases(function, args, result):
+    from dcspy.sdk import led_sdk
+    led_sdk.LED_DLL = None
+    assert getattr(led_sdk, function)(*args) is result
