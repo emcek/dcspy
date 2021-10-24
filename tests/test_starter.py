8c4b2d300493134580b310594607f9c2bdbb1748
Michal Plichta <michal.plichta@nokia.com>
2021-10-24 21:35:36 +0200

    add test for _supporters

--- tests/test_starter.py
+++ tests/test_starter.py
@@ -1,9 +1,16 @@
 import socket
 
-from dcspy import starter
+
+def test_supporters():
+    from dcspy import starter
+    sup_iter = starter._supporters(text='123456', width=5)
+    assert next(sup_iter) == '12345'
+    assert next(sup_iter) == '23456'
+    assert next(sup_iter) == '34561'
 
 
 def test_prepare_socket():
+    from dcspy import starter
     sock = starter._prepare_socket()
     assert isinstance(sock, socket.socket)
     assert sock.proto == 17
