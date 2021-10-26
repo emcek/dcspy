beeb6280e5e00b6b97fe18ffeab1794d523c2e6e
Michal Plichta <michal.plichta@nokia.com>
2021-10-27 00:01:56 +0200

    define extensive dataclass for LCD
    (cherry picked from commit 4d8ccb2da461d1f0ad0bed30e7f8441210b5d9bb)

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from logging import getLogger
 from os import name
 from platform import architecture, uname, python_implementation, python_version
@@ -19,6 +20,42 @@ LcdInfo = NamedTuple('LcdInfo', [('width', int), ('height', int), ('type', int),
                                  ('fg', Union[int, Sequence[int]]), ('bg', Union[int, Sequence[int]])])
 LcdMono = LcdInfo(width=lcd_sdk.MONO_WIDTH, height=lcd_sdk.MONO_HEIGHT, type=lcd_sdk.TYPE_MONO, fg=255, bg=0)
 LcdColor = LcdInfo(width=lcd_sdk.COLOR_WIDTH, height=lcd_sdk.COLOR_HEIGHT, type=lcd_sdk.TYPE_COLOR, fg=(0, 255, 0, 255), bg=(0, 0, 0, 0))
+
+
+@dataclass
+class GeneralLcd:
+    width: int
+    height: int
+    type: int
+    fg: Union[int, Sequence[int]]
+    bg: Union[int, Sequence[int]]
+    mode: str
+    s_font: Sequence[str, int]
+    l_font: Sequence[str, int]
+
+
+class MonoLcd(GeneralLcd):
+    width = lcd_sdk.MONO_WIDTH
+    height = lcd_sdk.MONO_HEIGHT
+    type = lcd_sdk.TYPE_MONO
+    fg = 255
+    bg = 0
+    mode = '1'
+    s_font: ('consola.ttf', 11)
+    l_font: ('consola.ttf', 22)
+
+
+class ColorLcd(GeneralLcd):
+    width = lcd_sdk.COLOR_WIDTH
+    height = lcd_sdk.COLOR_HEIGHT
+    type = lcd_sdk.TYPE_COLOR
+    fg = (0, 255, 0, 255)
+    bg = (0, 0, 0, 0)
+    mode = 'RGBA'
+    s_font: ('consola.ttf', 16)
+    l_font: ('consola.ttf', 32)
+
+
 LCD_TYPES = {'G19': 'KeyboardColor', 'G510': 'KeyboardMono', 'G15 v1/v2': 'KeyboardMono', 'G13': 'KeyboardMono'}
 LOG = getLogger(__name__)
 config_logger(LOG)
