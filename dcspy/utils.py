a962611f3af6cc6fc09799d47d7da715a893e773
Michal Plichta <michal.plichta@nokia.com>
2021-10-27 01:30:05 +0200

    back old code for format in python3.6

--- dcspy/utils.py
+++ dcspy/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from logging import getLogger
 from os import environ, makedirs
-from sys import prefix
+from sys import prefix, version_info
 from typing import Dict, Union, List, Tuple
 
 from packaging import version
@@ -89,7 +89,8 @@ def check_ver_at_github(repo: str, current_ver: str) -> Tuple[bool, str, str, st
             dict_json = response.json()
             online_version = dict_json['tag_name']
             pre_release = dict_json['prerelease']
-            frmt = '%Y-%m-%dT%H:%M:%S%z'
+            # quick hack for python3.6 - time zone has different code
+            frmt = '%Y-%m-%dT%H:%M:%S%z' if version_info.minor > 6 else '%Y-%m-%dT%H:%M:%SZ'
             published = datetime.strptime(dict_json['published_at'], frmt).strftime('%d %B %Y')
             asset_url = dict_json['assets'][0]['browser_download_url']
             LOG.debug(f'Latest GitHub version:{online_version} pre:{pre_release} date:{published} url:{asset_url}')
