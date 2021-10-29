266990187fc8588316da42e14e03bfe7b1a050b4
Michal Plichta <michal.plichta@nokia.com>
2021-10-29 18:32:26 +0200

    skip using additional font member in Lcd

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -3,7 +3,7 @@ from logging import getLogger
 from os import name
 from platform import architecture, uname, python_implementation, python_version
 from sys import platform
-from typing import Union, Sequence, Tuple, Dict
+from typing import Union, Sequence
 
 from PIL import ImageFont
 
@@ -26,23 +26,18 @@ class LcdInfo:
     fg: Union[int, Sequence[int]]
     bg: Union[int, Sequence[int]]
     mode: str
-    s_font: Tuple[str, str, int]
-    l_font: Tuple[str, str, int]
-    font: Dict[str, ImageFont.FreeTypeFont] = field(init=False)
-
-    def __post_init__(self):
-        # todo: secure when font is not found
-        self.font = {size_str: ImageFont.truetype(font_name, size_int)
-                     for size_str, font_name, size_int in (self.s_font, self.l_font)}
+    font_s: ImageFont.FreeTypeFont
+    font_l: ImageFont.FreeTypeFont
 
 
+# todo: secure when font is not found
 FONT_NAME = 'consola.ttf' if platform == 'win32' else 'DejaVuSans.ttf'
 FONT = {size: ImageFont.truetype(FONT_NAME, size) for size in (11, 16, 22, 32)}
 
 LcdMono = LcdInfo(width=lcd_sdk.MONO_WIDTH, height=lcd_sdk.MONO_HEIGHT, type=lcd_sdk.TYPE_MONO, fg=255,
-                  bg=0, mode='1', s_font=('S', FONT_NAME, 11), l_font=('L', FONT_NAME, 11))
+                  bg=0, mode='1', font_s=ImageFont.truetype(FONT_NAME, 11), font_l=ImageFont.truetype(FONT_NAME, 22))
 LcdColor = LcdInfo(width=lcd_sdk.COLOR_WIDTH, height=lcd_sdk.COLOR_HEIGHT, type=lcd_sdk.TYPE_COLOR, fg=(0, 255, 0, 255),
-                   bg=(0, 0, 0, 0), mode='RGBA', s_font=('S', FONT_NAME, 16), l_font=('L', FONT_NAME, 32))
+                   bg=(0, 0, 0, 0), mode='RGBA', font_s=ImageFont.truetype(FONT_NAME, 16), font_l=ImageFont.truetype(FONT_NAME, 32))
 
 LCD_TYPES = {'G19': 'KeyboardColor', 'G510': 'KeyboardMono', 'G15 v1/v2': 'KeyboardMono', 'G13': 'KeyboardMono'}
 LOG = getLogger(__name__)
