a951f1eafccb7203121983b9ea1999eef3de6be9
Michal Plichta <michal.plichta@nokia.com>
2021-11-02 00:53:41 +0100

    optimze imports

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from logging import getLogger
 from os import name
 from platform import architecture, uname, python_implementation, python_version
