db1a61fca6775d6df40a2048d314efee3cc8d2f9
Michal Plichta <michal.plichta@nokia.com>
2021-12-31 16:28:16 +0100

    define type in constructor

--- dcspy/tk_gui.py
+++ dcspy/tk_gui.py
@@ -36,8 +36,8 @@ class DcspyGui(tk.Frame):
         self.status_txt = tk.StringVar()
         self.cfg_file = config_file
         self._init_widgets()
-        self.l_bios = version.Version('0.0.0')
-        self.r_bios = version.Version('0.0.0')
+        self.l_bios: Union[version.Version, version.LegacyVersion]
+        self.r_bios: Union[version.Version, version.LegacyVersion]
         self.bios_path = ''
         self.event = Event()
 
