4762768bd9fff63dcb0c68f21dafbd1410ae2709
Michal Plichta <michal.plichta@nokia.com>
2022-01-02 18:37:18 +0100

    fix test form BIOS_PROTO

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -7,10 +7,10 @@ from helpers import check_dcsbios_data
 
 
 @mark.parametrize('plane', ['hornet_mono', 'viper_mono', 'black_shark_mono', 'warthog_mono', 'tomcat_mono', 'harrier_mono'])
-def test_bios_values_all_planes(plane, request):
+def test_bios_proto_values_all_planes(plane, request):
     plane = request.getfixturevalue(plane)
     name = SUPPORTED_CRAFTS[plane.__class__.__name__]
-    results, dcsbios_ver = check_dcsbios_data(plane.bios_data, f'{name}.json')
+    results, dcsbios_ver = check_dcsbios_data(plane.bios_proto, f'{name}.json')
     print(f'\n{name} BIOS {dcsbios_ver}\n{"-" * (len(name) + 13)}')
     pprint(results if results else 'No issues found', width=100)
     assert not results
