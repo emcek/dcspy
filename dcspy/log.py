0211b3d6949a5dd7192bc8bd02c90b6539fbf674
Michal Plichta <michal.plichta@nokia.com>
2022-06-19 22:31:02 +0200

    add new parameter

--- dcspy/log.py
+++ dcspy/log.py
@@ -4,11 +4,12 @@ from logging.handlers import RotatingFileHandler
 from os import environ, path
 
 
-def config_logger(logger: Logger) -> None:
+def config_logger(logger: Logger, verbose: bool) -> None:
     """
     Configure global logger add handlers and set formatters.
 
     :param logger:
+    :param verbose: turn on/off verbose mode
     """
     logger.setLevel(DEBUG)
     file_hand = RotatingFileHandler(filename=path.join(environ.get('TEMP', ''), 'dcspy.log'), mode='a', encoding='utf-8', maxBytes=5 * 1024 * 1024, backupCount=1)
@@ -16,6 +17,8 @@ def config_logger(logger: Logger) -> None:
     file_hand.setFormatter(Formatter('%(asctime)s | %(name)-17s | %(levelname)-7s | %(threadName)-10s | %(message)s / %(funcName)s:%(lineno)d'))
     stream_hand = StreamHandler()
     stream_hand.setLevel(INFO)
+    if verbose:
+        stream_hand.setLevel(DEBUG)
     stream_hand.setFormatter(Formatter('%(levelname)-7s | %(message)s'))
     logger.addHandler(stream_hand)
     logger.addHandler(file_hand)
