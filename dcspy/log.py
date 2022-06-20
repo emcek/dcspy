73717cb1f2c1a5e1530cf55a6d7ba6afe5543f98
Michal Plichta <michal.plichta@nokia.com>
2022-06-20 11:05:32 +0200

    use gettempdir() logger configuration

--- dcspy/log.py
+++ dcspy/log.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from logging import DEBUG, StreamHandler, INFO, Formatter, Logger
 from logging.handlers import RotatingFileHandler
-from os import environ, path
+from os import path
+from tempfile import gettempdir
 
 
 def config_logger(logger: Logger, verbose=False) -> None:
@@ -12,7 +13,7 @@ def config_logger(logger: Logger, verbose=False) -> None:
     :param verbose: turn on/off verbose mode
     """
     logger.setLevel(DEBUG)
-    file_hand = RotatingFileHandler(filename=path.join(environ.get('TEMP', ''), 'dcspy.log'), mode='a', encoding='utf-8', maxBytes=5 * 1024 * 1024, backupCount=1)
+    file_hand = RotatingFileHandler(filename=path.join(gettempdir(), 'dcspy.log'), mode='a', encoding='utf-8', maxBytes=5 * 1024 * 1024, backupCount=1)
     file_hand.setLevel(DEBUG)
     file_hand.setFormatter(Formatter('%(asctime)s | %(name)-17s | %(levelname)-7s | %(threadName)-10s | %(message)s / %(funcName)s:%(lineno)d'))
     stream_hand = StreamHandler()
