d6ff12f7efb9f2978a4f18f16214ec96fb8abb4e
Michal Plichta <michal.plichta@nokia.com>
2022-07-04 11:58:43 +0200

    add buttons to <lcdInfo dataclass

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -44,6 +44,7 @@ class LcdInfo:
     width: int
     height: int
     type: LcdType
+    buttons: Sequence[LcdButton]
     foreground: Union[int, Sequence[int]]
     background: Union[int, Sequence[int]]
     mode: str
@@ -55,10 +56,12 @@ class LcdInfo:
 config = set_defaults(load_cfg())
 
 LcdMono = LcdInfo(width=lcd_sdk.MONO_WIDTH, height=lcd_sdk.MONO_HEIGHT, type=LcdType.mono, foreground=255,
+                  buttons=(LcdButton.one, LcdButton.two, LcdButton.three, LcdButton.four),
                   background=0, mode='1', font_s=ImageFont.truetype(config['font_name'], config['font_mono_s']),
                   font_l=ImageFont.truetype(config['font_name'], config['font_mono_l']),
                   font_xs=ImageFont.truetype(config['font_name'], config['font_mono_xs']))
 LcdColor = LcdInfo(width=lcd_sdk.COLOR_WIDTH, height=lcd_sdk.COLOR_HEIGHT, type=LcdType.color, foreground=(0, 255, 0, 255),
+                   buttons=(LcdButton.left, LcdButton.right, LcdButton.up, LcdButton.down, LcdButton.ok, LcdButton.cancel, LcdButton.menu),
                    background=(0, 0, 0, 0), mode='RGBA', font_s=ImageFont.truetype(config['font_name'], config['font_color_s']),
                    font_l=ImageFont.truetype(config['font_name'], config['font_color_l']),
                    font_xs=ImageFont.truetype(config['font_name'], config['font_color_xs']))
