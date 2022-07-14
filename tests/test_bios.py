7c4be750e70ebd72f5b00d914953c6e1e34163ce
Michal Plichta <michal.plichta@nokia.com>
2022-07-14 23:44:53 +0200

    use updated fixture

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -6,7 +6,7 @@ from dcspy import SUPPORTED_CRAFTS
 from tests.helpers import check_dcsbios_data
 
 
-@mark.parametrize('plane', ['hornet_mono', 'viper_mono', 'black_shark_mono', 'warthog_mono', 'tomcat_mono', 'harrier_mono', 'apache_mono'])
+@mark.parametrize('plane', ['hornet_mono', 'viper_mono', 'shark_mono', 'warthog_mono', 'tomcat_mono', 'harrier_mono', 'apache_mono'])
 def test_bios_values_all_planes(plane, request):
     plane = request.getfixturevalue(plane)
     name = SUPPORTED_CRAFTS[plane.__class__.__name__]
