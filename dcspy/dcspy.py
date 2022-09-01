2d1238e1f8080cba963da02129f5b14b063b26b9
Michal Plichta <michal.plichta@nokia.com>
2022-09-01 19:58:52 +0200

    Merge branch 'master' into led_support
    # Conflicts:
    #	CONTRIBUTING.md
    #	dcspy/aircraft.py
    #	dcspy/utils.py
    #	resources/config.yaml
    #	tests/test_aircraft.py
    #	tests/test_bios.py
    #	tests/test_led_sdk.py
    #	tests/test_logitech.py
    #	tests/test_utils.py
    #	uml/classes.puml

--- dcspy/dcspy.py
+++ dcspy/dcspy.py
@@ -1,26 +1,30 @@
 import tkinter as tk
 from logging import getLogger
-from sys import prefix
+from os import path
 from threading import Event
 
 from dcspy import config, LCD_TYPES
 from dcspy.starter import dcspy_run
 from dcspy.tk_gui import DcspyGui
+from dcspy.utils import check_dcs_ver
 
 LOG = getLogger(__name__)
-__version__ = '1.6.0'
+__version__ = '1.7.3'
 
 
 def run():
     """Function to start DCSpy GUI."""
     if config['show_gui']:
         LOG.info(f'dcspy {__version__} https://github.com/emcek/dcspy')
+        dcs_type, dcs_ver = check_dcs_ver(config["dcs"])
+        LOG.info(f'DCS {dcs_type} ver: {dcs_ver}')
         root = tk.Tk()
         width, height = 210, 160
         root.geometry(f'{width}x{height}')
         root.minsize(width=width, height=height)
-        root.iconbitmap(f'{prefix}/dcspy_data/dcspy.ico')
-        gui = DcspyGui(master=root, config_file=f'{prefix}/dcspy_data/config.yaml')
+        here = path.abspath(path.dirname(__file__))
+        root.iconbitmap(default=path.join(here, 'dcspy.ico'))
+        gui = DcspyGui(master=root, config_file=path.join(here, 'config.yaml'))
         gui.mainloop()
     else:
         dcspy_run(lcd_type=LCD_TYPES[config['keyboard']], event=Event())
