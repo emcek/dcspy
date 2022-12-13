62ab77a1bf0733232f3fc8b44374e9f1e1bd4c75
Michal Plichta <mplichta@gmail.com>
2022-12-13 10:50:34 +0100

    fix detection of darkmode for icon

--- dcspy/run.py
+++ dcspy/run.py
@@ -27,7 +27,7 @@ def run():
         root.minsize(width=width, height=height)
         here = path.abspath(path.dirname(__file__))
         root.iconbitmap(path.join(here, 'dcspy.ico'))
-        if config['theme_mode'] == 'Dark':
+        if config['theme_mode'] == 'dark':
             root.iconbitmap(path.join(here, 'dcspy_white.ico'))
         root.title('DCSpy')
         DcspyGui(master=root, config_file=path.join(here, 'config.yaml'))
