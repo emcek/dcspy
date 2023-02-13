259e43da40df54d27eb3a744dccc27552100bb7a
Michal Plichta <mplichta@gmail.com>
2023-02-13 16:36:14 +0100

    update version 1.9.0

--- dcspy/starter.py
+++ dcspy/starter.py
@@ -14,7 +14,7 @@ from dcspy.utils import get_version_string
 
 LOG = getLogger(__name__)
 LOOP_FLAG = True
-__version__ = '1.9.2'
+__version__ = '1.9.0'
 
 
 def _handle_connection(logi_keyboard: LogitechKeyboard, parser: ProtocolParser, sock: socket.socket, event: Event) -> None:
