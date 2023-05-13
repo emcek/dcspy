08d85e55bd26c3401437f15998da9811653149cf
Michal Plichta <mplichta@gmail.com>
2023-05-14 01:14:14 +0200

    update unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -142,7 +142,7 @@ def test_check_github_repo(tmpdir):
     sha = utils.check_github_repo(git_ref='branch', update=True, repo='emcek/dcspy', repo_dir=tmpdir)
     match = search(r'([0-9a-f]{8})\sfrom:\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}\sby:\s.*', sha)
     assert match.group(1)
-    sha = utils.check_github_repo(git_ref='branch', update=False, repo='emcek/dcspy', repo_dir=tmpdir)
+    sha = utils.check_github_repo(git_ref='master', update=False, repo='emcek/dcspy', repo_dir=tmpdir)
     match = search(r'([0-9a-f]{8})\sfrom:\s\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}', sha)
     assert match.group(1)
 
