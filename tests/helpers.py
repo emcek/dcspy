315a046e76fb0f50808a19e7ec151adf43fdc820
Michal Plichta <mplichta@gmail.com>
2023-05-18 20:12:14 +0200

    add compare_images as help[er

--- tests/helpers.py
+++ tests/helpers.py
@@ -5,11 +5,12 @@ from tempfile import gettempdir
 from typing import Tuple, List, Union, Dict
 from unittest.mock import patch
 
+import PIL
+from PIL import ImageChops
 from requests import get, exceptions
 
 from dcspy import aircraft, BiosValue
 from dcspy.sdk import lcd_sdk
-
 
 all_plane_list = ['FA18Chornet', 'F16C50', 'Ka50', 'Ka503', 'Mi8MT', 'Mi24P', 'AH64DBLKII', 'A10C', 'A10C2', 'F14A135GR', 'F14B', 'AV8BNA']
 
@@ -179,3 +180,18 @@ def set_bios_during_test(aircraft_model: aircraft.Aircraft, bios_pairs: List[Tup
                 patch.object(lcd_sdk, 'logi_lcd_update', return_value=True):
             for selector, value in bios_pairs:
                 aircraft_model.set_bios(selector, value)
+
+
+def compare_images(img: PIL.Image.Image, file_path: Path) -> bool:
+    """
+    Comparing generated image with saved file.
+
+    :param img: Generated image
+    :param file_path: path to reference image
+    :return: Treu if images are the same
+    """
+    instance = isinstance(img, PIL.Image.Image)
+    ref_img = PIL.Image.open(file_path)
+    all_bytes = img.tobytes() == ref_img.tobytes()
+    pixel_diff = not ImageChops.difference(img, ref_img).getbbox()
+    return all([instance, all_bytes, pixel_diff])
