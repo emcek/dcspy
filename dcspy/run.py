e2652451a790c047d294cc4bba6f1450659d8473
Michal Plichta <mplichta@gmail.com>
2023-05-22 23:23:59 +0200

    draft of system tray icon

--- dcspy/run.py
+++ dcspy/run.py
@@ -1,8 +1,11 @@
+from functools import partial
 from logging import getLogger
 from pathlib import Path
 from threading import Event
 
 import customtkinter
+from PIL import Image
+from pystray import Icon, MenuItem
 
 from dcspy import LCD_TYPES, config
 from dcspy.starter import dcspy_run
@@ -11,6 +14,42 @@ from dcspy.utils import check_dcs_ver
 
 LOG = getLogger(__name__)
 __version__ = '2.0.0'
+
+
+def _quit_window(window: customtkinter.CTk, icon: Icon, menu: MenuItem):
+    """
+    Quit application.
+
+    :param window: main window
+    :param icon: icon instance
+    :param menu: menu item instance
+    """
+    LOG.debug(f'Icon: {icon.name}, Menu: {menu.text}')
+    icon.visible = False
+    icon.stop()
+    window.quit()
+    window.destroy()
+
+
+def _show_window(window: customtkinter.CTk, icon: Icon, menu: MenuItem):
+    """
+    Show main application window.
+
+    :param window: main window
+    :param icon: icon instance
+    :param menu: menu item instance
+    """
+    LOG.debug(f'Icon: {icon.name}, Menu: {menu.text}')
+    window.after(0, window.deiconify)
+
+
+def _withdraw_window(window: customtkinter.CTk):
+    """
+    Withdraw main application window.
+
+    :param window: main window
+    """
+    window.withdraw()
 
 
 def run():
@@ -29,6 +68,13 @@ def run():
         if config['theme_mode'] == 'dark':
             root.iconbitmap(Path(__file__).resolve().with_name('dcspy_white.ico'))
         root.title('DCSpy')
+
+        image = Image.open(Path(__file__).resolve().with_name('dcspy.ico'))
+        menu = (MenuItem('Quit', partial(_quit_window, root)), MenuItem('Show', partial(_show_window, root)))
+        icon = Icon('dcspy', image, 'DCSpy', menu)
+        icon.run_detached()
+        root.protocol('WM_DELETE_WINDOW', partial(_withdraw_window, root))
+
         DcspyGui(master=root)
         root.mainloop()
     else:
