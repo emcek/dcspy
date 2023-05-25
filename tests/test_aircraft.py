00803b5c7eeab80503293cdc1b768153def88f80
Michal Plichta <mplichta@gmail.com>
2023-05-25 22:14:38 +0200

    typo

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -265,7 +265,7 @@ def test_set_bios_for_airplane(plane, bios_pairs, result, request):
     ('apache_color', [('PLT_EUFD_LINE1', '                  |AFT FUEL LOW      |TAIL WHL LOCK SEL ')], 'IDM'),
     ('apache_color', [('PLT_EUFD_LINE1', 'ENGINE 1 OUT      |AFT FUEL LOW      |PRESET TUNE FM1 ')], 'PRE'),
 ], ids=['Mono IDM', 'Mono PRE', 'Color IDM 1', 'Color IDM 2', 'Color PRE'])
-def test_apatch_mode_switch_idm_pre_for_apache(plane, bios_pairs, mode, request):
+def test_apache_mode_switch_idm_pre_for_apache(plane, bios_pairs, mode, request):
     plane = request.getfixturevalue(plane)
     set_bios_during_test(plane, bios_pairs)
     assert plane.mode.name == mode
