4165f7896ed8041cfb6652c46b5d1cc6e78de3b6
Michal Plichta <mplichta@gmail.com>
2023-06-23 13:16:02 +0200

    add eagle to bios test

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -7,10 +7,12 @@ from tests.helpers import check_dcsbios_data, generate_bios_data_for_plane
 
 
 @mark.dcsbios
-@mark.parametrize('plane', ['hornet_mono', 'viper_mono', 'shark_mono', 'hip_mono', 'hind_mono', 'warthog_mono', 'tomcatb_mono', 'harrier_mono', 'apache_mono'])
+@mark.parametrize('plane', ['hornet_mono', 'viper_mono', 'eagle_mono', 'shark_mono', 'hip_mono', 'hind_mono', 'warthog_mono', 'tomcatb_mono', 'harrier_mono', 'apache_mono'])
 def test_bios_values_all_planes(plane, request):
     plane = request.getfixturevalue(plane)
     name = SUPPORTED_CRAFTS[type(plane).__name__]['bios']
+    if 'F15' in type(plane).__name__:
+        name = 'F-15E'
     results, dcsbios_ver = check_dcsbios_data(plane_bios=plane.bios_data, plane_json=f'{name}.json', git_bios=True)
     print(f'\n{name} BIOS {dcsbios_ver}\n{"-" * (len(name) + 13)}')
     pprint(results if results else 'No issues found', width=100)
