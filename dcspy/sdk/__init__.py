42b3b8fa3b33b6db0e3a64ad25214804138dcb18
Michal Plichta <mplichta@gmail.com>
2023-08-09 00:50:35 +0200

    add DllSdk

--- dcspy/sdk/__init__.py
+++ dcspy/sdk/__init__.py
@@ -1,16 +1,89 @@
 from ctypes import c_void_p, sizeof
+from dataclasses import dataclass
 from logging import getLogger
 from os import environ
 from platform import architecture
 from sys import maxsize
 from typing import Optional
 
+from _cffi_backend import Lib
 from cffi import FFI
 
 LOG = getLogger(__name__)
 
 
-def _init_dll(lib_type: str) -> object:
+@dataclass
+class DllSdk:
+    """DLL SDK."""
+    name: str
+    header: str
+
+
+lcd_header = '''
+bool LogiLcdInit(wchar_t* friendlyName, int lcdType);
+bool LogiLcdIsConnected(int lcdType);
+bool LogiLcdIsButtonPressed(int button);
+void LogiLcdUpdate();
+void LogiLcdShutdown();
+
+// Monochrome LCD functions
+bool LogiLcdMonoSetBackground(BYTE monoBitmap[]);
+bool LogiLcdMonoSetText(int lineNumber, wchar_t* text);
+
+// Color LCD functions
+bool LogiLcdColorSetBackground(BYTE colorBitmap[]);
+bool LogiLcdColorSetTitle(wchar_t* text, int red, int green, int blue);
+bool LogiLcdColorSetText(int lineNumber, wchar_t* text, int red, int green, int blue);
+'''
+led_header = '''
+bool LogiLedInit();
+bool LogiLedInitWithName(const char name[]);
+
+bool LogiLedGetSdkVersion(int *majorNum, int *minorNum, int *buildNum);
+bool LogiLedGetConfigOptionNumber(const wchar_t *configPath, double *defaultValue);
+bool LogiLedGetConfigOptionBool(const wchar_t *configPath, bool *defaultValue);
+bool LogiLedGetConfigOptionColor(const wchar_t *configPath, int *defaultRed, int *defaultGreen, int *defaultBlue);
+bool LogiLedGetConfigOptionRect(const wchar_t *configPath, int *defaultX, int *defaultY, int *defaultWidth, int *defaultHeight);
+bool LogiLedGetConfigOptionString(const wchar_t *configPath, wchar_t *defaultValue, int bufferSize);
+bool LogiLedGetConfigOptionKeyInput(const wchar_t *configPath, wchar_t *defaultValue, int bufferSize);
+bool LogiLedGetConfigOptionSelect(const wchar_t *configPath, wchar_t *defaultValue, int *valueSize, const wchar_t *values, int bufferSize);
+bool LogiLedGetConfigOptionRange(const wchar_t *configPath, int *defaultValue, int min, int max);
+bool LogiLedSetConfigOptionLabel(const wchar_t *configPath, wchar_t *label);
+
+//Generic functions => Apply to any device type.
+bool LogiLedSetTargetDevice(int targetDevice);
+bool LogiLedSaveCurrentLighting();
+bool LogiLedSetLighting(int redPercentage, int greenPercentage, int bluePercentage);
+bool LogiLedRestoreLighting();
+bool LogiLedFlashLighting(int redPercentage, int greenPercentage, int bluePercentage, int milliSecondsDuration, int milliSecondsInterval);
+bool LogiLedPulseLighting(int redPercentage, int greenPercentage, int bluePercentage, int milliSecondsDuration, int milliSecondsInterval);
+bool LogiLedStopEffects();
+
+//Per-key functions => only apply to LOGI_DEVICETYPE_PERKEY_RGB devices.
+bool LogiLedSetLightingFromBitmap(unsigned char bitmap[]);
+bool LogiLedSetLightingForKeyWithScanCode(int keyCode, int redPercentage, int greenPercentage, int bluePercentage);
+bool LogiLedSetLightingForKeyWithHidCode(int keyCode, int redPercentage, int greenPercentage, int bluePercentage);
+bool LogiLedSetLightingForKeyWithQuartzCode(int keyCode, int redPercentage, int greenPercentage, int bluePercentage);
+bool LogiLedSetLightingForKeyWithKeyName(LogiLed::KeyName keyName, int redPercentage, int greenPercentage, int bluePercentage);
+bool LogiLedSaveLightingForKey(LogiLed::KeyName keyName);
+bool LogiLedRestoreLightingForKey(LogiLed::KeyName keyName);
+bool LogiLedExcludeKeysFromBitmap(LogiLed::KeyName *keyList, int listCount);
+
+//Per-key effects => only apply to LOGI_DEVICETYPE_PERKEY_RGB devices.
+bool LogiLedFlashSingleKey(LogiLed::KeyName keyName, int redPercentage, int greenPercentage, int bluePercentage, int msDuration, int msInterval);
+bool LogiLedPulseSingleKey(LogiLed::KeyName keyName, int startRedPercentage, int startGreenPercentage, int startBluePercentage, int finishRedPercentage, int finishGreenPercentage, int finishBluePercentage, int msDuration, bool isInfinite);
+bool LogiLedStopEffectsOnKey(LogiLed::KeyName keyName);
+
+//Zonal functions => only apply to devices with zones.
+bool LogiLedSetLightingForTargetZone(LogiLed::DeviceType deviceType, int zone, int redPercentage, int greenPercentage, int bluePercentage);
+
+void LogiLedShutdown();
+'''
+LCD = DllSdk(name='LCD', header=lcd_header)
+LED = DllSdk(name='LED', header=led_header)
+
+
+def _init_dll(lib_type: DllSdk) -> Lib:
     """
     Initialize C dynamic linking library.
 
@@ -22,29 +95,15 @@ def _init_dll(lib_type: str) -> object:
         prog_files = environ['PROGRAMW6432']
     except KeyError:
         prog_files = environ['PROGRAMFILES']
-    dll_path = f"{prog_files}\\Logitech Gaming Software\\SDK\\{lib_type}\\{arch}\\Logitech{lib_type.capitalize()}.dll"
+    dll_path = f"{prog_files}\\Logitech Gaming Software\\SDK\\{lib_type.name}\\{arch}\\Logitech{lib_type.name.capitalize()}.dll"
     LOG.debug(f'Selected DLL: {dll_path}')
     ffi = FFI()
-    # dll = CDLL(dll_path)
-    ffi.cdef('''
-bool LogiLcdInit(wchar_t* friendlyName, int lcdType);
-bool LogiLcdIsConnected(int lcdType);
-bool LogiLcdIsButtonPressed(int button);
-void LogiLcdUpdate();
-void LogiLcdShutdown();
-
-bool LogiLcdMonoSetBackground(BYTE monoBitmap[]);
-bool LogiLcdMonoSetText(int lineNumber, wchar_t* text);
-
-bool LogiLcdColorSetBackground(BYTE colorBitmap[]);
-bool LogiLcdColorSetTitle(wchar_t* text, int red, int green, int blue);
-bool LogiLcdColorSetText(int lineNumber, wchar_t* text, int red, int green, int blue);
-    ''')
+    ffi.cdef(lib_type.header)
     dll = ffi.dlopen(dll_path)
     return dll
 
 
-def load_dll(lib_type: str) -> Optional[object]:
+def load_dll(lib_type: DllSdk) -> Optional[Lib]:
     """
     Initialize and load of C dynamic linking library.
 
@@ -53,10 +112,10 @@ def load_dll(lib_type: str) -> Optional[object]:
     """
     try:
         dll = _init_dll(lib_type)
-        LOG.info(f'Loading of {lib_type} SDK success')
+        LOG.info(f'Loading of {lib_type.name} SDK success')
         return dll
     except (KeyError, OSError) as err:
         header = '*' * 44
-        LOG.error(f'\n{header}\n*{type(err).__name__:^42}*\n{header}\nLoading of {lib_type} SDK failed !', exc_info=True)
+        LOG.error(f'\n{header}\n*{type(err).__name__:^42}*\n{header}\nLoading of {lib_type.name} SDK failed !', exc_info=True)
         LOG.error(f'{header}')
         return None
