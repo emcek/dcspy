2cdb0d9725b5fa30c54177a68c3d3f64d2c6c554
Michal Plichta <mplichta@gmail.com>
2023-08-12 23:57:15 +0200

    clean up key_sdk module

--- dcspy/sdk/key_sdk.py
+++ dcspy/sdk/key_sdk.py
@@ -59,31 +59,3 @@ def logi_gkey_shutdown() -> None:
         KEY_DLL.LogiGkeyShutdown()
     except AttributeError:
         pass
-
-
-def check_button_pressed():
-    """Check if GKey was pressed."""
-    try:
-        while True:
-            for key in range(1, LOGITECH_MAX_GKEYS):
-                for state in range(1, LOGITECH_MAX_M_STATES):
-                    gkey = get_gkey_name(key, state)
-                    LOG.debug(f"Button {gkey} is pressed")
-            sleep(0.1)
-    except KeyboardInterrupt:
-        logi_gkey_shutdown()
-
-
-def get_gkey_name(key: int, state: int) -> str:
-    """
-    Get string for specified G-key and Mode number.
-
-    :param key: number of the G-key to check (for example between 1 and 29 for G13).
-    :param state: number of the mode currently selected (1, 2 or 3)
-    :return: Friendly string for specified G-key and Mode number. For example 'G5/M1'.
-    """
-    gkey = ''
-    if logi_gkey_is_keyboard_gkey_pressed(g_key=key, mode=state):
-        gkey = logi_gkey_is_keyboard_gkey_string(key, state)
-        print(f"Button {gkey.split('/')} is pressed")
-    return gkey
