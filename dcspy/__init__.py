7dcf2ad226d1b72a4d0868b794fbfc6f8e060067
Michal Plichta <mplichta@gmail.com>
2023-08-12 12:41:19 +0200

    add draft LcdGkey and update LcdInfo

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from enum import Enum
+from enum import Enum, auto
 from logging import getLogger
 from os import name
 from pathlib import Path
@@ -88,6 +88,97 @@ class LcdButton(Enum):
     MENU = 0x4000
 
 
+class LcdGkey(Enum):
+    G1_M1 = 11
+    G2_M1 = 21
+    G3_M1 = 31
+    G4_M1 = 41
+    G5_M1 = 51
+    G6_M1 = 61
+    G7_M1 = 71
+    G8_M1 = 81
+    G9_M1 = 91
+    G10_M1 = 101
+    G11_M1 = 111
+    G12_M1 = 121
+    G13_M1 = 131
+    G14_M1 = 141
+    G15_M1 = 151
+    G16_M1 = 161
+    G17_M1 = 171
+    G18_M1 = 181
+    G19_M1 = 191
+    G20_M1 = 201
+    G21_M1 = 211
+    G22_M1 = 221
+    G23_M1 = 231
+    G24_M1 = 241
+    G25_M1 = 251
+    G26_M1 = 261
+    G27_M1 = 271
+    G28_M1 = 281
+    G29_M1 = 291
+    G1_M2 = 12
+    G2_M2 = 22
+    G3_M2 = 32
+    G4_M2 = 42
+    G5_M2 = 52
+    G6_M2 = 62
+    G7_M2 = 72
+    G8_M2 = 82
+    G9_M2 = 92
+    G10_M2 = 102
+    G11_M2 = 112
+    G12_M2 = 122
+    G13_M2 = 132
+    G14_M2 = 142
+    G15_M2 = 152
+    G16_M2 = 162
+    G17_M2 = 172
+    G18_M2 = 182
+    G19_M2 = 192
+    G20_M2 = 202
+    G21_M2 = 212
+    G22_M2 = 222
+    G23_M2 = 232
+    G24_M2 = 242
+    G25_M2 = 252
+    G26_M2 = 262
+    G27_M2 = 272
+    G28_M2 = 282
+    G29_M2 = 292
+    G1_M3 = 13
+    G2_M3 = 23
+    G3_M3 = 33
+    G4_M3 = 43
+    G5_M3 = 53
+    G6_M3 = 63
+    G7_M3 = 73
+    G8_M3 = 83
+    G9_M3 = 93
+    G10_M3 = 103
+    G11_M3 = 113
+    G12_M3 = 123
+    G13_M3 = 133
+    G14_M3 = 143
+    G15_M3 = 153
+    G16_M3 = 163
+    G17_M3 = 173
+    G18_M3 = 183
+    G19_M3 = 193
+    G20_M3 = 203
+    G21_M3 = 213
+    G22_M3 = 223
+    G23_M3 = 233
+    G24_M3 = 243
+    G25_M3 = 253
+    G26_M3 = 263
+    G27_M3 = 273
+    G28_M3 = 283
+    G29_M3 = 293
+    NONE = ''
+
+
 class LcdMode(Enum):
     """LCD Mode."""
     BLACK_WHITE = '1'
@@ -101,6 +192,7 @@ class LcdInfo:
     height: int
     type: LcdType
     buttons: Sequence[LcdButton]
+    gkey: Sequence[LcdGkey]
     foreground: Union[int, Tuple[int, int, int, int]]
     background: Union[int, Tuple[int, int, int, int]]
     mode: LcdMode
@@ -113,11 +205,13 @@ default_yaml = get_default_yaml(local_appdata=LOCAL_APPDATA)
 config = set_defaults(load_cfg(filename=default_yaml), filename=default_yaml)
 LcdMono = LcdInfo(width=MONO_WIDTH, height=MONO_HEIGHT, type=LcdType.MONO, foreground=255,
                   buttons=(LcdButton.ONE, LcdButton.TWO, LcdButton.THREE, LcdButton.FOUR),
+                  gkey=(LcdGkey.G1_M1, LcdGkey.G2_M1, LcdGkey.G8_M1, LcdGkey.G9_M1),
                   background=0, mode=LcdMode.BLACK_WHITE, font_s=ImageFont.truetype(str(config['font_name']), int(config['font_mono_s'])),
                   font_l=ImageFont.truetype(str(config['font_name']), int(config['font_mono_l'])),
                   font_xs=ImageFont.truetype(str(config['font_name']), int(config['font_mono_xs'])))
 LcdColor = LcdInfo(width=COLOR_WIDTH, height=COLOR_HEIGHT, type=LcdType.COLOR, foreground=(0, 255, 0, 255),
                    buttons=(LcdButton.LEFT, LcdButton.RIGHT, LcdButton.UP, LcdButton.DOWN, LcdButton.OK, LcdButton.CANCEL, LcdButton.MENU),
+                   gkey=(LcdGkey.G1_M1, LcdGkey.G2_M1, LcdGkey.G8_M1, LcdGkey.G9_M1),
                    background=(0, 0, 0, 0), mode=LcdMode.TRUE_COLOR, font_s=ImageFont.truetype(str(config['font_name']), int(config['font_color_s'])),
                    font_l=ImageFont.truetype(str(config['font_name']), int(config['font_color_l'])),
                    font_xs=ImageFont.truetype(str(config['font_name']), int(config['font_color_xs'])))
