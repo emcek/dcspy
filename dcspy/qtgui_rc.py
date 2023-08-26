08278ab7f20f8bf6764e06b36bb93616bbd3226d
Michal Plichta <mplichta@gmail.com>
2023-08-27 01:52:19 +0200

    add extra new lines

--- dcspy/qtgui_rc.py
+++ dcspy/qtgui_rc.py
@@ -5913,10 +5913,13 @@ qt_resource_struct = b"\
 \x00\x00\x01\x8a4\x15nw\
 "
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
