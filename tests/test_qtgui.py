7729bbcf96ab1f8da93031f39b58a265c725f11e
Michal Plichta <mplichta@gmail.com>
2023-08-27 04:27:17 +0200

    move import inside test

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -1,14 +1,14 @@
 from sys import platform
 
-from PySide6.QtCore import Qt
 from pytest import mark
-
-from dcspy.qt_gui import DcsPyQtGui
 
 
 @mark.qt6
 @mark.skipif(condition=platform != 'win32', reason='Run only on Windows')
 def test_qt(qtbot):
+    from PySide6.QtCore import Qt
+
+    from dcspy.qt_gui import DcsPyQtGui
     dcspy_gui = DcsPyQtGui()
     dcspy_gui.show()
     qtbot.addWidget(dcspy_gui)
