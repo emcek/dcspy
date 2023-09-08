fd3143767a8708b75fd3362a990e0f593743f824
Michal Plichta <mplichta@gmail.com>
2023-09-08 15:17:29 +0200

    print qt-test

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -9,7 +9,9 @@ def test_qt(qtbot):
     from PySide6.QtCore import Qt
 
     from dcspy.qt_gui import DcsPyQtGui
+    print('DUPA')
     dcspy_gui = DcsPyQtGui()
     dcspy_gui.show()
+    print(dcspy_gui.config)
     qtbot.addWidget(dcspy_gui)
     qtbot.mouseClick(dcspy_gui.pb_close, Qt.LeftButton)
