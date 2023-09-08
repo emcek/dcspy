4977ed4391a71259a2129d581d25688afc07ab8d
Michal Plichta <mplichta@gmail.com>
2023-09-08 16:37:14 +0200

    configure GUI for test

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -10,18 +10,13 @@ def test_qt(qtbot):
     from PySide6.QtCore import Qt
 
     from dcspy.qt_gui import DcsPyQtGui
-    cfg_dict = {'keyboard': 'G13', 'save_lcd': False, 'show_gui': True, 'autostart': False,
-                'dcsbios': f'D:\\Users\\{environ.get("USERNAME", "UNKNOWN")}\\Saved Games\\DCS.openbeta\\Scripts\\DCS-BIOS',
-                'dcs': 'C:\\Program Files\\Eagle Dynamics\\DCS World OpenBeta', 'verbose': False, 'check_bios': False, 'check_ver': False,
-                'font_name': 'consola.ttf', 'font_mono_s': 11, 'font_mono_xs': 9, 'font_mono_l': 16, 'font_color_s': 22, 'font_color_xs': 18,
-                'font_color_l': 32, 'f16_ded_font': True, 'git_bios': False, 'git_bios_ref': 'master', 'theme_mode': 'system', 'theme_color': 'dark-blue'}
+
+    cfg_dict = {'dcsbios': f'D:\\Users\\{environ.get("USERNAME", "UNKNOWN")}\\Saved Games\\DCS.openbeta\\Scripts\\DCS-BIOS',
+                'dcs': 'C:\\Program Files\\Eagle Dynamics\\DCS World OpenBeta', 'keyboard': 'G13', 'save_lcd': False, 'show_gui': True, 'autostart': False,
+                'verbose': False, 'check_bios': False, 'check_ver': False, 'font_name': 'consola.ttf', 'font_mono_s': 11, 'font_mono_xs': 9, 'font_mono_l': 16,
+                'font_color_s': 22, 'font_color_xs': 18, 'font_color_l': 32, 'f16_ded_font': True, 'git_bios': False, 'git_bios_ref': 'master',
+                'theme_mode': 'system', 'theme_color': 'dark-blue'}
     dcspy_gui = DcsPyQtGui(cfg_dict=cfg_dict)
-    print(dcspy_gui.cfg_file)
-    with open(dcspy_gui.cfg_file) as f:
-        d = f.read()
-    print(d)
-    print(dcspy_gui.cb_autoupdate_bios.isChecked())
-    print(dcspy_gui.config)
     dcspy_gui.show()
     qtbot.addWidget(dcspy_gui)
     qtbot.mouseClick(dcspy_gui.pb_close, Qt.LeftButton)
