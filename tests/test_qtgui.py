f65ce65e51a419759b8655a2640e83b1d1a4ed2b
Michal Plichta <mplichta@gmail.com>
2023-09-08 15:39:17 +0200

    print qt-test 3

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -9,9 +9,9 @@ def test_qt(qtbot):
     from PySide6.QtCore import Qt
 
     from dcspy.qt_gui import DcsPyQtGui
-    print('DUPA')
     dcspy_gui = DcsPyQtGui()
-    dcspy_gui.show()
     print(dcspy_gui.config)
+    print(dcspy_gui.cb_autoupdate_bios.isChecked())
+    dcspy_gui.show()
     qtbot.addWidget(dcspy_gui)
     qtbot.mouseClick(dcspy_gui.pb_close, Qt.LeftButton)
