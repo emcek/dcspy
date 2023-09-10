d87f62f3a780c998aa5ebbc667f7c5e4ac8f3653
Michal Plichta <mplichta@gmail.com>
2023-09-10 22:06:01 +0200

    improve completer and disable items in combo with text '--'

--- dcspy/qt_gui.py
+++ dcspy/qt_gui.py
@@ -16,7 +16,7 @@ from webbrowser import open_new_tab
 import qtawesome
 from packaging import version
 from PySide6 import QtCore, QtUiTools
-from PySide6.QtGui import QAction, QIcon, QPixmap
+from PySide6.QtGui import QAction, QIcon, QPixmap, QStandardItem, QStandardItemModel
 from PySide6.QtWidgets import (QCheckBox, QComboBox, QCompleter, QDockWidget, QFileDialog, QLabel, QLineEdit, QMainWindow, QMenu, QMessageBox, QProgressBar,
                                QPushButton, QRadioButton, QSlider, QSpinBox, QStatusBar, QSystemTrayIcon, QTableWidget, QTabWidget, QToolBar, QWidget)
 
@@ -245,129 +245,6 @@ class DcsPyQtGui(QMainWindow):
 
     def _load_table_gkeys(self) -> None:
         """Initialize table with cockpit data."""
-        n1 = ['ADI_AUX_FLAG', 'ADI_BANK', 'ADI_BUBBLE', 'ADI_GS_BAR', 'ADI_GS_FLAG', 'ADI_GS_POINTER', 'ADI_LOC_BAR',
-              'ADI_LOC_FLAG', 'ADI_OFF_FLAG',
-              'ADI_PITCH', 'ADI_PITCH_TRIM', 'ADI_TURNRATE', 'AIRSPEED', 'AIRSPEED_SET_KNB', 'MACH_INDICATOR',
-              'MAX_AIRSPEED', 'SET_AIRSPEED',
-              'ALT_10000_FT_CNT', 'ALT_1000_FT_CNT', 'ALT_100_FT_CNT', 'ALT_100_FT_PTR', 'ALT_BARO_SET_KNB',
-              'ALT_MODE_LV', 'ALT_PNEU_FLAG',
-              'ALT_PRESSURE_DRUM_0_CNT', 'ALT_PRESSURE_DRUM_1_CNT', 'ALT_PRESSURE_DRUM_2_CNT',
-              'ALT_PRESSURE_DRUM_3_CNT', 'AOA_VALUE', 'COMM1_MODE_KNB',
-              'COMM1_PWR_KNB', 'COMM2_MODE_KNB', 'COMM2_PWR_KNB', 'HOT_MIC_SW', 'IFF_ANT_SEL_SW', 'ILS_PWR_KNB',
-              'INTERCOM_KNB', 'MSL_KNB', 'SEC_VOICE_KNB',
-              'TACAN_KNB', 'TF_KNB', 'THREAT_KNB', 'UHF_ANT_SEL_SW', 'VMS_INHIBIT_SW', 'DL_SW', 'GPS_SW', 'INS_KNB',
-              'MAP_SW', 'MFD_SW', 'MIDS_LVT_KNB',
-              'MMC_PWR_SW', 'ST_STA_SW', 'UFC_SW', 'CMDS_01_EXP_CAT_SW', 'CMDS_02_EXP_CAT_SW', 'CMDS_CH_Amount',
-              'CMDS_CH_EXP_CAT_SW', 'CMDS_DISPENSE_BTN',
-              'CMDS_FL_Amount', 'CMDS_FL_EXP_CAT_SW', 'CMDS_JETT_SW', 'CMDS_JMR_SOURCHE_SW', 'CMDS_MODE_KNB',
-              'CMDS_MWS_SOURCHE_SW', 'CMDS_O1_Amount',
-              'CMDS_O2_Amount', 'CMDS_PROG_KNB', 'CMDS_PWR_SOURCHE_SW', 'CLOCK_CURRTIME_H', 'CLOCK_CURRTIME_MS',
-              'CLOCK_ELAPSED', 'CLOCK_ELAPSED_TIME_M',
-              'CLOCK_ELAPSED_TIME_SEC', 'CLOCK_SET', 'CLOCK_WIND', 'CANOPY_HANDLE', 'CANOPY_JETT_THANDLE', 'CANOPY_POS',
-              'CANOPY_SW', 'HIDE_STICK', 'SEAT_ADJ',
-              'SEAT_EJECT_HANDLE', 'SEAT_EJECT_SAFE', 'SEAT_HEIGHT', 'ADV_MODE_SW', 'ALT_FLAPS_SW', 'AP_PITCH_SW',
-              'AP_ROLL_SW', 'BIT_SW', 'DIGI_BAK_SW',
-              'FLCS_RESET_SW', 'LE_FLAPS_SW', 'MANUAL_PITCH_SW', 'MAN_TF_FLYUP_SW', 'PITCH_TRIM', 'ROLL_TRIM',
-              'STORES_CONFIG_SW', 'TRIM_AP_DISC_SW',
-              'YAW_TRIM', 'DED_LINE_1', 'DED_LINE_2', 'DED_LINE_3', 'DED_LINE_4', 'DED_LINE_5', 'ECM_1_BTN',
-              'ECM_2_BTN', 'ECM_3_BTN', 'ECM_4_BTN', 'ECM_5_BTN',
-              'ECM_6_BTN', 'ECM_BIT_BTN', 'ECM_DIM_KNB', 'ECM_FRM_BTN', 'ECM_PW_SW', 'ECM_RESET_BTN', 'ECM_SPL_BTN',
-              'ECM_XMIT_SW', 'AIR_SOURCE_KNB',
-              'DEFOG_LEVER', 'TEMP_KNB', 'EHSI_CRS_SET', 'EHSI_CRS_SET_KNB', 'EHSI_HDG_SET_BTN', 'EHSI_HDG_SET_KNB',
-              'EHSI_MODE', 'EPU_SW', 'EPU_SW_COVER_OFF',
-              'EPU_SW_COVER_ON', 'HYDRAZIN_VOLUME', 'ELEC_CAUTION', 'EPU_GEN_TEST_SW', 'FLCS_PWR_TEST_SW',
-              'MAIN_PWR_SW', 'PROBE_HEAT_SW', 'AB_RESET_SW',
-              'ENGINE_FTIT', 'ENGINE_NOZZLE_POSITION', 'ENGINE_OIL_PRESSURE', 'ENGINE_TACHOMETER', 'ENG_ANTI_ICE',
-              'ENG_CONT_SW', 'ENG_CONT_SW_COVER',
-              'FIRE_OHEAT_DETECT_BTN', 'JFS_SW', 'MAX_PWR_SW', 'EXT_FORMATION_LIGHTS', 'EXT_POSITION_LIGHTS_WING',
-              'EXT_POSITION_LIGHT_FUSELAGE',
-              'EXT_SPEED_BRAKE_LEFT', 'EXT_SPEED_BRAKE_RIGHT', 'EXT_STROBE_TAIL', 'EXT_TAIL_LIGHT', 'EXT_WOW_LEFT',
-              'EXT_WOW_NOSE', 'EXT_WOW_RIGHT',
-              'AIR_REFUEL_LIGHT_KNB', 'ANTI_COLL_LIGHT_KNB', 'ENGINE_FEED_KNB', 'FORM_LIGHT_KNB', 'LAND_TAXI_LIGHT_SW',
-              'MAL_IND_LTS_BRT_SW', 'MASTER_LIGHT_SW',
-              'POS_FLASH_LIGHT_SW', 'POS_FUSELAGE_LIGHT_SW', 'POS_WING_TAIL_LIGHT_SW', 'AIR_REFUEL_SW',
-              'EXT_FUEL_TRANS_SW', 'FUELFLOWCOUNTER_100',
-              'FUELFLOWCOUNTER_10K', 'FUELFLOWCOUNTER_1K', 'FUELTOTALIZER_100', 'FUELTOTALIZER_10K', 'FUELTOTALIZER_1K',
-              'FUEL_AL', 'FUEL_FR', 'FUEL_MASTER_CV',
-              'FUEL_MASTER_SW', 'FUEL_QTY_SEL_KNB', 'FUEL_QTY_SEL_T_KNB', 'TANK_INTERTING_SW', 'ANTI_SKID_SW',
-              'BRAKE_CHAN_SW', 'DN_LOCK_BTN', 'GEAR_ALT_BTN',
-              'GEAR_ALT_HANDLE', 'GEAR_HANDLE', 'HOOK_SW', 'HORN_SILENCE_BTN', 'HMCS_INT_KNB', 'HUD_ALT_SW',
-              'HUD_BRT_SW', 'HUD_DED_DATA_SW',
-              'HUD_DEPRESS_RET_SW', 'HUD_FP_MARKER_SW', 'HUD_SCALES_SW', 'HUD_SPEED_SW', 'HUD_TEST_SW', 'SYSA_PRESSURE',
-              'SYSB_PRESSURE',
-              'IFF_CODE_DRUM_DIGIT_1', 'IFF_CODE_DRUM_DIGIT_2', 'IFF_CODE_DRUM_DIGIT_3', 'IFF_CODE_DRUM_DIGIT_4',
-              'IFF_C_I_KNB', 'IFF_ENABLE_SW',
-              'IFF_M1_SEL_1', 'IFF_M1_SEL_2', 'IFF_M3_SEL_1', 'IFF_M3_SEL_2', 'IFF_M4_CODE_SW', 'IFF_M4_MONITOR_SW',
-              'IFF_M4_REPLY_SW', 'IFF_MASTER_KNB',
-              'AOA_INDEX_BRT_KNB', 'AR_STATUS_BRT_KNB', 'FLOOD_CONSOLES_BRT_KNB', 'FLOOD_INST_PNL_BRT_KNB',
-              'MAL_IND_LTS_TEST', 'MASTER_CAUTION',
-              'PRI_CONSOLES_BRT_KNB', 'PRI_DATA_DISPLAY_BRT_KNB', 'PRI_INST_PNL_BRT_KNB', 'LIGHT_CONSLES',
-              'LIGHT_CONSLES_FLOOD', 'LIGHT_INST_PNL',
-              'LIGHT_INST_PNL_FLOOD', 'KY58_FILL_KNB', 'KY58_MODE_KNB', 'KY58_PWR_KNB', 'KY58_VOL_KNB',
-              'PLAIN_CIPHER_SW', 'ZEROIZE_SW', 'ZEROIZE_SW_COVER',
-              'MFD_L_1', 'MFD_L_10', 'MFD_L_11', 'MFD_L_12', 'MFD_L_13', 'MFD_L_14', 'MFD_L_15', 'MFD_L_16', 'MFD_L_17',
-              'MFD_L_18', 'MFD_L_19', 'MFD_L_2',
-              'MFD_L_20', 'MFD_L_3', 'MFD_L_4', 'MFD_L_5', 'MFD_L_6', 'MFD_L_7', 'MFD_L_8', 'MFD_L_9', 'MFD_L_BRT_SW',
-              'MFD_L_CON_SW', 'MFD_L_GAIN_SW',
-              'MFD_L_SYM_SW', 'MFD_R_1', 'MFD_R_10', 'MFD_R_11', 'MFD_R_12', 'MFD_R_13', 'MFD_R_14', 'MFD_R_15',
-              'MFD_R_16', 'MFD_R_17', 'MFD_R_18', 'MFD_R_19',
-              'MFD_R_2', 'MFD_R_20', 'MFD_R_3', 'MFD_R_4', 'MFD_R_5', 'MFD_R_6', 'MFD_R_7', 'MFD_R_8', 'MFD_R_9',
-              'MFD_R_BRT_SW', 'MFD_R_CON_SW',
-              'MFD_R_GAIN_SW', 'MFD_R_SYM_SW', 'ALT_REL_BTN', 'EMERG_STORE_JETT', 'GND_JETT_ENABLE_SW', 'LASER_ARM_SW',
-              'MASTER_ARM_SW', 'COCKPIT_ALITITUDE',
-              'FLOW_INDICATOR', 'FLOW_INDICATOR_LIGHT', 'OBOGS_SW', 'OXYGEN_PRESSURE', 'OXY_DILUTER_LVR',
-              'OXY_EMERG_LVR', 'OXY_SUPPLY_LVR', 'RWR_ACT_PWR_BTN',
-              'RWR_ALT_BTN', 'RWR_HANDOFF_BTN', 'RWR_IND_DIM_KNB', 'RWR_INTENS_KNB', 'RWR_LAUNCH_BTN', 'RWR_MODE_BTN',
-              'RWR_PWR_BTN', 'RWR_SEARCH_BTN',
-              'RWR_SYS_TEST_BTN', 'RWR_T_BTN', 'RWR_UNKNOWN_SHIP_BTN', 'SAI_AIRCRAFTREFERENCESYMBOL', 'SAI_BANK',
-              'SAI_BANK_ARROW', 'SAI_CAGE', 'SAI_KNB_ARROW',
-              'SAI_OFF_FLAG', 'SAI_PITCH', 'SAI_PITCH_TRIM', 'FCR_PWR_SW', 'HDPT_SW_L', 'HDPT_SW_R', 'RDR_ALT_PWR_SW',
-              'SPEEDBRAKE_INDICATOR', 'PITCHTRIMIND',
-              'ROLLTRIMIND', 'F_ACK_BTN', 'ICP_AA_MODE_BTN', 'ICP_AG_MODE_BTN', 'ICP_BTN_0', 'ICP_BTN_1', 'ICP_BTN_2',
-              'ICP_BTN_3', 'ICP_BTN_4', 'ICP_BTN_5',
-              'ICP_BTN_6', 'ICP_BTN_7', 'ICP_BTN_8', 'ICP_BTN_9', 'ICP_COM1_BTN', 'ICP_COM2_BTN', 'ICP_DATA_RTN_SEQ_SW',
-              'ICP_DATA_UP_DN_SW', 'ICP_DED_SW',
-              'ICP_DRIFT_SW', 'ICP_ENTR_BTN', 'ICP_FLIR_GAIN_SW', 'ICP_FLIR_SW', 'ICP_HUD_BRT_KNB', 'ICP_IFF_BTN',
-              'ICP_LIST_BTN', 'ICP_RASTER_BRT_KNB',
-              'ICP_RASTER_CONTR_KNB', 'ICP_RCL_BTN', 'ICP_RETICLE_DEPRESS_KNB', 'ICP_WX_BTN', 'IFF_ID_BTN', 'RF_SW',
-              'UHF_CHAN_DISP', 'UHF_CHAN_KNB',
-              'UHF_DOOR', 'UHF_FREQ_0025_KNB', 'UHF_FREQ_01_KNB', 'UHF_FREQ_100_KNB', 'UHF_FREQ_10_KNB',
-              'UHF_FREQ_1_KNB', 'UHF_FREQ_DISP', 'UHF_FUNC_KNB',
-              'UHF_MODE_KNB', 'UHF_SQUELCH_SW', 'UHF_STATUS_BTN', 'UHF_TEST_BTN', 'UHF_TONE_BTN', 'UHF_VOL_KNB', 'VVI',
-              'LIGHT_ACFT_BATT_FAIL', 'LIGHT_ACTIVE',
-              'LIGHT_AFT_FUEL_LOW', 'LIGHT_AIR', 'LIGHT_ANTI_SKID', 'LIGHT_AOA_DN', 'LIGHT_AOA_MID', 'LIGHT_AOA_UP',
-              'LIGHT_AR_NWS', 'LIGHT_ATF_NOT',
-              'LIGHT_AVIONICS_FAULT', 'LIGHT_BUC', 'LIGHT_CABIN_PRESS', 'LIGHT_CADC', 'LIGHT_CANOPY', 'LIGHT_CAUTION_1',
-              'LIGHT_CAUTION_2', 'LIGHT_CAUTION_3',
-              'LIGHT_CAUTION_4', 'LIGHT_CAUTION_5', 'LIGHT_CAUTION_6', 'LIGHT_CMDS_DISP', 'LIGHT_CMDS_GO',
-              'LIGHT_CMDS_NO_GO', 'LIGHT_CMDS_RDY', 'LIGHT_DBU_ON',
-              'LIGHT_DISC', 'LIGHT_ECM', 'LIGHT_ECM_1_A', 'LIGHT_ECM_1_F', 'LIGHT_ECM_1_S', 'LIGHT_ECM_1_T',
-              'LIGHT_ECM_2_A', 'LIGHT_ECM_2_F', 'LIGHT_ECM_2_S',
-              'LIGHT_ECM_2_T', 'LIGHT_ECM_3_A', 'LIGHT_ECM_3_F', 'LIGHT_ECM_3_S', 'LIGHT_ECM_3_T', 'LIGHT_ECM_4_A',
-              'LIGHT_ECM_4_F', 'LIGHT_ECM_4_S',
-              'LIGHT_ECM_4_T', 'LIGHT_ECM_5_A', 'LIGHT_ECM_5_F', 'LIGHT_ECM_5_S', 'LIGHT_ECM_5_T', 'LIGHT_ECM_A',
-              'LIGHT_ECM_F', 'LIGHT_ECM_FRM_A',
-              'LIGHT_ECM_FRM_F', 'LIGHT_ECM_FRM_S', 'LIGHT_ECM_FRM_T', 'LIGHT_ECM_S', 'LIGHT_ECM_SPL_A',
-              'LIGHT_ECM_SPL_F', 'LIGHT_ECM_SPL_S',
-              'LIGHT_ECM_SPL_T', 'LIGHT_ECM_T', 'LIGHT_EDGE', 'LIGHT_EEC', 'LIGHT_ELEC', 'LIGHT_ELEC_SYS',
-              'LIGHT_ENGINE', 'LIGHT_ENGINE_FAULT',
-              'LIGHT_ENG_FIRE', 'LIGHT_EPU', 'LIGHT_EPU_GEN', 'LIGHT_EPU_PMG', 'LIGHT_EQUIP_HOT', 'LIGHT_FLCS',
-              'LIGHT_FLCS_FAULT', 'LIGHT_FLCS_PMG',
-              'LIGHT_FLCS_PWR_A', 'LIGHT_FLCS_PWR_B', 'LIGHT_FLCS_PWR_C', 'LIGHT_FLCS_PWR_D', 'LIGHT_FLCS_RLY',
-              'LIGHT_FL_FAIL', 'LIGHT_FL_RUN',
-              'LIGHT_FUEL_OIL_HOT', 'LIGHT_FWD_FUEL_LOW', 'LIGHT_GEAR_L', 'LIGHT_GEAR_N', 'LIGHT_GEAR_R',
-              'LIGHT_GEAR_WARN', 'LIGHT_HOOK', 'LIGHT_HYDRAZN',
-              'LIGHT_HYD_OIL_PRESS', 'LIGHT_IFF', 'LIGHT_INLET_ICING', 'LIGHT_JFS_RUN', 'LIGHT_MAIN_GEN',
-              'LIGHT_MARKER_BEACON', 'LIGHT_MASTER_CAUTION',
-              'LIGHT_NUCLEAR', 'LIGHT_NWS_FAIL', 'LIGHT_OBOGS', 'LIGHT_OVERHEAT', 'LIGHT_OXY_LOW', 'LIGHT_PROBE_HEAT',
-              'LIGHT_RADAR_ALT', 'LIGHT_RDY',
-              'LIGHT_RWR_ACTIVITY', 'LIGHT_RWR_ACT_POWER', 'LIGHT_RWR_ALT', 'LIGHT_RWR_ALT_LOW', 'LIGHT_RWR_HANDOFF_H',
-              'LIGHT_RWR_HANDOFF_UP',
-              'LIGHT_RWR_MODE_OPEN', 'LIGHT_RWR_MODE_PRI', 'LIGHT_RWR_MSL_LAUNCH', 'LIGHT_RWR_POWER',
-              'LIGHT_RWR_SEARCH', 'LIGHT_RWR_SHIP_UNK',
-              'LIGHT_RWR_SYSTEST', 'LIGHT_RWR_TGTSEP_DN', 'LIGHT_RWR_TGTSEP_UP', 'LIGHT_SEAT_NOT', 'LIGHT_SEC',
-              'LIGHT_STBY', 'LIGHT_STBY_GEN',
-              'LIGHT_STORES_CONFIG', 'LIGHT_TF_FAIL', 'LIGHT_TO_FLCS', 'LIGHT_TO_LDG_CONFIG']
         n2 = ['', '-- ADI --', 'ADI_AUX_FLAG', 'ADI_BANK', 'ADI_BUBBLE', 'ADI_GS_BAR', 'ADI_GS_FLAG', 'ADI_GS_POINTER',
               'ADI_LOC_BAR', 'ADI_LOC_FLAG',
               'ADI_OFF_FLAG', 'ADI_PITCH', 'ADI_PITCH_TRIM', 'ADI_TURNRATE', '-- Airspeed Indicator --', 'AIRSPEED',
@@ -517,7 +394,7 @@ class DcsPyQtGui(QMainWindow):
 
         for r in range(0, self.keyboard.gkeys):
             for c in range(0, self.keyboard.modes):
-                completer = QCompleter(n1)
+                completer = QCompleter([item for item in n2 if item and '--' not in item])
                 completer.setCaseSensitivity(QtCore.Qt.CaseSensitivity.CaseInsensitive)
                 completer.setCompletionMode(QCompleter.CompletionMode.PopupCompletion)
                 completer.setFilterMode(QtCore.Qt.MatchFlag.MatchContains)
@@ -528,8 +405,22 @@ class DcsPyQtGui(QMainWindow):
                 combo.setEditable(True)
                 combo.addItems(n2)
                 combo.setCompleter(completer)
+                self._disable_items_with(text='--', combo=combo)
                 self.tw_gkeys.setCellWidget(r, c, combo)
                 self.tw_gkeys.cellWidget(r, c).setCurrentText(plane_gkeys.get(f'G{r + 1}_M{c + 1}', ''))
+
+    @staticmethod
+    def _disable_items_with(text: str, combo: QComboBox) -> None:
+        """
+        Disable items in ComboBox, which shouldn't be selected.
+
+        :param combo: QComboBox instance
+        """
+        model = combo.model()
+        for i in range(1, combo.count()):
+            item: QStandardItem = model.item(i)
+            if text in item.text():
+                item.setEnabled(False)
 
     def _save_gkeys_cfg(self) -> None:
         """Save G-Keys configuration for current plane."""
