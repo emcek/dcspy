91a9ed019809bab98cf764bd5d2e0cf31513be5b
Michal Plichta <mplichta@gmail.com>
2023-09-15 23:08:06 +0200

    improve qt test

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -1,30 +1,23 @@
-from pathlib import Path
 from sys import platform
+from unittest.mock import patch
 
 from pytest import mark
-
-resources = Path(__file__).resolve().with_name('resources')
 
 
 @mark.qt6
 @mark.skipif(condition=platform != 'win32', reason='Run only on Windows')
-def test_qt(qtbot, default_config):
+def test_qt(qtbot, resources, switch_dcs_bios_path_in_config):
     from PySide6 import QtCore
     from PySide6.QtCore import Qt
     from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
 
-    from dcspy.qt_gui import DcsPyQtGui
+    from dcspy import qt_gui
 
     QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGLRhi)
     QtCore.QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
 
-    default_config.update({
-        'check_bios': False,
-        'check_ver': False,
-        'dcsbios': str(resources / 'dcs_bios'),
-        'current_plane': 'A-10C',
-    })
-    dcspy_gui = DcsPyQtGui(cfg_dict=default_config)
+    with patch.object(qt_gui, 'get_default_yaml', return_value=resources / 'c.yml'):
+        dcspy_gui = qt_gui.DcsPyQtGui()
     dcspy_gui.show()
     qtbot.addWidget(dcspy_gui)
     qtbot.mouseClick(dcspy_gui.pb_close, Qt.LeftButton)
