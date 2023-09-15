99b6aa4aa7b1288098096c86e18d25559f6b0691
Michal Plichta <mplichta@gmail.com>
2023-09-15 20:24:02 +0200

    fix qt test

--- tests/test_qtgui.py
+++ tests/test_qtgui.py
@@ -1,20 +1,22 @@
 from pathlib import Path
 from sys import platform
 
-from PySide6 import QtCore
-from PySide6.QtCore import Qt
-from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
 from pytest import mark
 
 resources = Path(__file__).resolve().with_name('resources')
-QtCore.QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
-QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGLRhi)
 
 
 @mark.qt6
 @mark.skipif(condition=platform != 'win32', reason='Run only on Windows')
 def test_qt(qtbot, default_config):
+    from PySide6 import QtCore
+    from PySide6.QtCore import Qt
+    from PySide6.QtQuick import QQuickWindow, QSGRendererInterface
+
     from dcspy.qt_gui import DcsPyQtGui
+
+    QQuickWindow.setGraphicsApi(QSGRendererInterface.OpenGLRhi)
+    QtCore.QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
 
     default_config.update({
         'check_bios': False,
