6370b30adc6bde1546edf2683a6b08613391f1db
Michal Plichta <mplichta@gmail.com>
2023-09-16 03:24:54 +0200

    test only mono planes

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -3,15 +3,13 @@ from pprint import pprint
 from pytest import mark
 
 from dcspy import SUPPORTED_CRAFTS
-from tests.helpers import (all_plane_list, check_dcsbios_data,
-                           generate_bios_data_for_plane)
+from tests.helpers import check_dcsbios_data, generate_bios_data_for_plane
 
 
 @mark.dcsbios
-@mark.parametrize('lcd', ['mono', 'color'])
-@mark.parametrize('plane', all_plane_list)
+@mark.parametrize('plane', ['fa18chornet', 'f16c50', 'f15ese', 'ka50', 'mi8mt', 'mi24p', 'ah64dblkii', 'a10c', 'f14b', 'av8bna'])
 def test_bios_values_all_planes(plane, lcd, request):
-    plane = request.getfixturevalue(f'{plane}_{lcd}')
+    plane = request.getfixturevalue(f'{plane}_mono')
     name = SUPPORTED_CRAFTS[type(plane).__name__]['bios']
     results, dcsbios_ver = check_dcsbios_data(plane_bios=plane.bios_data, plane_name=f'{name}', git_bios=True)
     print(f'\n{name} {lcd} BIOS {dcsbios_ver}\n{"-" * (len(name) + len(lcd) + 14)}')
