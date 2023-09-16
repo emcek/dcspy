d347de60376343a8554e4460d427b58c7f73a6f5
Michal Plichta <mplichta@gmail.com>
2023-09-16 03:39:33 +0200

    remove lcd from bios test

--- tests/test_bios.py
+++ tests/test_bios.py
@@ -8,11 +8,11 @@ from tests.helpers import check_dcsbios_data, generate_bios_data_for_plane
 
 @mark.dcsbios
 @mark.parametrize('plane', ['fa18chornet', 'f16c50', 'f15ese', 'ka50', 'mi8mt', 'mi24p', 'ah64dblkii', 'a10c', 'f14b', 'av8bna'])
-def test_bios_values_all_planes(plane, lcd, request):
+def test_bios_values_all_planes(plane, request):
     plane = request.getfixturevalue(f'{plane}_mono')
     name = SUPPORTED_CRAFTS[type(plane).__name__]['bios']
     results, dcsbios_ver = check_dcsbios_data(plane_bios=plane.bios_data, plane_name=f'{name}', git_bios=True)
-    print(f'\n{name} {lcd} BIOS {dcsbios_ver}\n{"-" * (len(name) + len(lcd) + 14)}')
+    print(f'\n{name} BIOS {dcsbios_ver}\n{"-" * (len(name) + 13)}')
     pprint(results if results else 'No issues found', width=100)
     if results:
         print('----- Full BIOS entry -----')
