bf9087ce4c4591a5b5958f743142f618ddba47c0
Michal Plichta <mplichta@gmail.com>
2023-10-15 03:11:06 +0200

    remove BiosValue

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -9,15 +9,6 @@ from dcspy.log import config_logger
 from dcspy.models import LOCAL_APPDATA
 from dcspy.utils import check_dcs_ver, get_default_yaml, load_yaml, set_defaults
 
-try:
-    from typing import NotRequired
-except ImportError:
-    from typing_extensions import NotRequired
-try:
-    from typing import TypedDict
-except ImportError:
-    from typing_extensions import TypedDict
-
 __version__ = '3.0.0'
 
 default_yaml = get_default_yaml(local_appdata=LOCAL_APPDATA)
@@ -44,21 +35,3 @@ def get_config_yaml_item(key: str, /, default: Optional[Union[str, int]] = None)
     :return: value from configuration
     """
     return load_yaml(full_path=default_yaml).get(key, default)
-
-
-class IntBuffArgs(TypedDict):
-    address: int
-    mask: int
-    shift_by: int
-
-
-class StrBuffArgs(TypedDict):
-    address: int
-    max_length: int
-
-
-class BiosValue(TypedDict):
-    klass: str
-    args: Union[StrBuffArgs, IntBuffArgs]
-    value: Union[int, str]
-    max_value: NotRequired[int]
