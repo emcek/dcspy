ac252f4022a90d7cf0ddfb074066931091d256b5
Michal Plichta <mplichta@gmail.com>
2023-10-16 16:21:47 +0200

    add todo item

--- tests/test_logitech.py
+++ tests/test_logitech.py
@@ -179,7 +179,7 @@ def test_check_keyboard_text(lcd_font, keyboard, protocol_parser):
 @mark.parametrize('model', [
     'FA18Chornet', 'F16C50', 'F15ESE', 'Ka50', 'Ka503', 'Mi8MT', 'Mi24P', 'AH64DBLKII', 'A10C', 'A10C2', 'F14A135GR', 'F14B', 'AV8BNA',
 ])
-def test_keyboard_mono_load_plane(model, keyboard_mono):
+def test_keyboard_mono_load_plane(model, keyboard_mono):  # todo: need mock for Local data config Ka-50 i.e.
     from dcspy.aircraft import AdvancedAircraft
     from dcspy.sdk import lcd_sdk
     with patch.object(lcd_sdk, 'logi_lcd_is_connected', return_value=True), \
