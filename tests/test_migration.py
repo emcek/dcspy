c345171e254fbe38b7cf6fd034f5c60fc42bb3b5
Michal Plichta <mplichta@gmail.com>
2023-10-25 13:49:14 +0200

    fix test for migration

--- tests/test_migration.py
+++ tests/test_migration.py
@@ -12,5 +12,5 @@ from dcspy.migration import migrate
      {'api_ver': '3.0.0-rc1', 'completer_items': 20, 'current_plane': 'A-10C', 'font_color_m': 66, 'font_color_s': 18, 'font_mono_m': 11, 'font_mono_s': 9, 'val': 1}),
 ], ids=['API 2.9.9', 'API 3.0.0-rc1', 'API empty'])
 def test_migrate(cfg, result):
-    migrate(cfg=cfg)
-    assert cfg == result
+    migrated_cfg = migrate(cfg=cfg)
+    assert all(result[key] == migrated_cfg[key] for key in result)
