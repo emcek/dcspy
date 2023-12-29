a224c7e7501dfc15d1a285b574b07359db57553a
Michal Plichta <mplichta@gmail.com>
2023-12-29 14:00:31 +0100

    Replace image save with image comparison for tests

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -336,16 +336,15 @@ def test_apache_wca_more_then_one_screen_scrolled(model, resources, img_precisio
         apache.prepare_image()
     assert apache.warning_line == 3
     img = apache.prepare_image()
-    img.save(resources / platform / f'{model}_wca_mode_scroll_3.png')
-    # assert compare_images(img=img, file_path=resources / platform / f'{model}_wca_mode_scroll_3.png', precision=img_precision)
+    assert compare_images(img=img, file_path=resources / platform / f'{model}_wca_mode_scroll_3.png', precision=img_precision)
 
     for i in range(1, 3):
         apache.warning_line += 1
         apache.prepare_image()
 
     img = apache.prepare_image()
-    img.save(resources / platform / f'{model}_wca_mode_scroll_1.png')
     assert apache.warning_line == 1
+    assert compare_images(img=img, file_path=resources / platform / f'{model}_wca_mode_scroll_1.png', precision=img_precision)
 
 
 @mark.parametrize('model', ['ah64dblkii_mono', 'ah64dblkii_color'], ids=['Mono LCD', 'Color LCD'])
