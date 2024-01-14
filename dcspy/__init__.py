84b7e157f80dc34bbd3abed2f1e200064b86d7d7
Michal Plichta <mplichta@gmail.com>
2024-01-14 14:05:54 +0100

    move line

--- dcspy/__init__.py
+++ dcspy/__init__.py
@@ -10,6 +10,7 @@ from dcspy.migration import migrate
 from dcspy.models import LOCAL_APPDATA
 from dcspy.utils import check_dcs_ver, get_default_yaml, load_yaml, save_yaml
 
+LOG = getLogger(__name__)
 __version__ = '3.1.2'
 
 default_yaml = get_default_yaml(local_appdata=LOCAL_APPDATA)
