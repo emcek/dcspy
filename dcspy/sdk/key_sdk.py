34b1f251e5d8512ddff3ad0b0641faaec5af73d1
Michal Plichta <mplichta@gmail.com>
2024-02-14 12:33:14 +0100

    re-added all SDK functions

--- dcspy/sdk/key_sdk.py
+++ dcspy/sdk/key_sdk.py
@@ -67,13 +67,39 @@ class GkeySdkManager:
             self.KEY_DLL.LogiGkeyInit.restype = c_bool
             self.KEY_DLL.LogiGkeyInit.argtypes = [POINTER(logiGkeyCBContext)]
 
-            return self.KEY_DLL.LogiGkeyInit(self.gkeyContextPointer)  # type: ignore[attr-defined]
+            return self.KEY_DLL.LogiGkeyInit(self.gkeyContextPointer)
         except AttributeError:
             return False
 
     def logi_gkey_shutdown(self) -> None:
         """Unload the corresponding DLL and frees up any allocated resources."""
         try:
-            self.KEY_DLL.LogiGkeyShutdown()  # type: ignore[attr-defined]
+            self.KEY_DLL.LogiGkeyShutdown()
         except AttributeError:
             pass
+
+    def logi_gkey_is_keyboard_gkey_pressed(self, g_key: int, mode: int) -> bool:
+        """
+        Indicate whether a keyboard G-key is currently being pressed.
+
+        :param g_key: number of the G-key to check (for example between 1 and 29 for G13).
+        :param mode: number of the mode currently selected (1, 2 or 3)
+        :return: True if the specified G-key for the specified Mode is currently being pressed, False otherwise.
+        """
+        try:
+            return self.KEY_DLL.LogiGkeyIsKeyboardGkeyPressed(g_key, mode)
+        except AttributeError:
+            return False
+
+    def logi_gkey_is_keyboard_gkey_string(self, g_key: int, mode: int) -> str:
+        """
+        Return a G-key-specific friendly string.
+
+        :param g_key: number of the G-key to check (for example between 1 and 29 for G13).
+        :param mode: number of the mode currently selected (1, 2 or 3)
+        :return: Friendly string for specified G-key and Mode number. For example 'G5/M1'.
+        """
+        try:
+            return self.KEY_DLL.LogiGkeyGetKeyboardGkeyString(g_key, mode)
+        except AttributeError:
+            return ''
