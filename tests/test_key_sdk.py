1b4bef96388a2ed83eb8454141e5acb1cb8c9266
Michal Plichta <mplichta@gmail.com>
2024-02-19 11:39:00 +0100

    make key_sdk and GkeySdkManager test more verbose and type awere

--- tests/test_key_sdk.py
+++ tests/test_key_sdk.py
@@ -8,8 +8,11 @@ from pytest import mark
     ('logi_gkey_shutdown', (), None),
 ], ids=['init', 'is gkey pressed', 'is gkey string', 'shutdown'])
 def test_all_failure_cases(function, args, result):
-    from dcspy.sdk.key_sdk import GkeySdkManager
+    from dcspy.sdk.key_sdk import GkeyCode, GkeySdkManager
 
-    key_sdk = GkeySdkManager(gkey_callback_handler=lambda *args: None)
+    def gkey_callback(g_key_code: GkeyCode, gkey_or_button_str='', context=None) -> None:
+        print(g_key_code, gkey_or_button_str, context)
+
+    key_sdk = GkeySdkManager(callback=gkey_callback)
     key_sdk.KEY_DLL = None
     assert getattr(key_sdk, function)(*args) is result
