1f5dcde6064820c9b12194c261b15cf6e32d8967
Michal Plichta <mplichta@gmail.com>
2024-03-14 19:23:47 +0100

    Use Logitech LCD SDK in logitech

--- dcspy/logitech.py
+++ dcspy/logitech.py
@@ -13,8 +13,7 @@ from dcspy import dcsbios, get_config_yaml_item
 from dcspy.aircraft import BasicAircraft, MetaAircraft
 from dcspy.models import (KEY_DOWN, SEND_ADDR, SUPPORTED_CRAFTS, TIME_BETWEEN_REQUESTS, Gkey, KeyboardModel, LcdButton, LcdColor, LcdMono, ModelG13, ModelG15v1,
                           ModelG15v2, ModelG19, ModelG510)
-from dcspy.sdk import lcd_sdk
-from dcspy.sdk.key_sdk import GkeySdkManager
+from dcspy.sdk import key_sdk, lcd_sdk
 from dcspy.utils import get_full_bios_for_plane, get_planes_list
 
 LOG = getLogger(__name__)
@@ -53,10 +52,10 @@ class KeyboardManager:
         self.model = KeyboardModel(name='', klass='', modes=0, gkeys=0, lcdkeys=(LcdButton.NONE,), lcd='mono')
         self.gkey: Sequence[Gkey] = ()
         self.buttons: Sequence[LcdButton] = ()
-        lcd_sdk.logi_lcd_init('DCS World', self.lcd.type.value)
-        self.key_sdk = GkeySdkManager(self.gkey_callback_handler)
+        self.lcd_sdk = lcd_sdk.LcdSdkManager(name='DCS World', lcd_type=self.lcd.type.value)
+        self.key_sdk = key_sdk.GkeySdkManager(self.gkey_callback_handler)
         success = self.key_sdk.logi_gkey_init()
-        LOG.info(f'logitech gkey sdk initialised: {success}')
+        LOG.debug(f'G-Key is connected: {success}')
         self.plane = BasicAircraft(self.lcd)
         self.vert_space = 0
 
@@ -79,10 +78,9 @@ class KeyboardManager:
         :param message: List of strings to display, row by row.
         """
         self._display = message
-        lcd_sdk.update_display(self._prepare_image())
+        self.lcd_sdk.update_display(self._prepare_image())
 
-    @staticmethod
-    def text(message: List[str]) -> None:
+    def text(self, message: List[str]) -> None:
         """
         Display message at LCD.
 
@@ -90,7 +88,7 @@ class KeyboardManager:
         For G19 takes first 8 or fewer elements of list and display as 8 rows.
         :param message: List of strings to display, row by row.
         """
-        lcd_sdk.update_text(message)
+        self.lcd_sdk.update_text(message)
 
     def detecting_plane(self, value: str) -> None:
         """
@@ -133,7 +131,7 @@ class KeyboardManager:
         """
         self.plane_detected = False
         if self.plane_name in SUPPORTED_CRAFTS:
-            self.plane = getattr(import_module('dcspy.aircraft'), self.plane_name)(self.lcd)
+            self.plane = getattr(import_module('dcspy.aircraft'), self.plane_name)(self.lcd, update_display=self.lcd_sdk.update_display)
             LOG.debug(f'Dynamic load of: {self.plane_name} as AdvancedAircraft | BIOS: {self.plane.bios_name}')
             self._setup_plane_callback()
         else:
@@ -171,7 +169,7 @@ class KeyboardManager:
         :return: LcdButton enum of pressed button
         """
         for btn in self.buttons:
-            if lcd_sdk.logi_lcd_is_button_pressed(btn.value):
+            if self.lcd_sdk.logi_lcd_is_button_pressed(btn.value):
                 if not self.lcdbutton_pressed:
                     self.lcdbutton_pressed = True
                     return LcdButton(btn)
@@ -210,7 +208,7 @@ class KeyboardManager:
         :param true_clear:
         """
         LOG.debug(f'Clear LCD type: {self.lcd.type}')
-        lcd_sdk.clear_display(true_clear)
+        self.lcd_sdk.clear_display(true_clear)
 
     def _prepare_image(self) -> Image.Image:
         """
