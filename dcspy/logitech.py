555af4e93ef959df19016c17bccb62687dfd8d9c
Michal Plichta <mplichta@gmail.com>
2024-03-18 15:49:12 +0100

    move all code from old LogitechKeyboard to LogitechDevice

--- dcspy/logitech.py
+++ dcspy/logitech.py
@@ -31,6 +31,7 @@ class LogitechDevice:
     """
 
     def __init__(self, parser: dcsbios.ProtocolParser, sock: socket, **kwargs) -> None:
+        """General Logitech device."""
         dcsbios.StringBuffer(parser=parser, address=0x0, max_length=0x10, callback=partial(self.detecting_plane))
         self.parser = parser
         self.socket = sock
@@ -51,6 +52,98 @@ class LogitechDevice:
         self.plane = BasicAircraft(self.lcd)
         self.vert_space = 0
 
+    @property
+    def display(self) -> List[str]:
+        """
+        Get the latest text from LCD.
+
+        :return: list of strings with data, row by row
+        """
+        return self._display
+
+    @display.setter
+    def display(self, message: List[str]) -> None:
+        """
+        Display message as image at LCD.
+
+        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
+        For G19 takes first 8 or fewer elements of list and display as 8 rows.
+        :param message: List of strings to display, row by row.
+        """
+        self._display = message
+        if not self.skip_lcd:
+            self.lcd_sdk.update_display(self._prepare_image())
+
+    def text(self, message: List[str]) -> None:
+        """
+        Display message at LCD.
+
+        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
+        For G19 takes first 8 or fewer elements of list and display as 8 rows.
+        :param message: List of strings to display, row by row.
+        """
+        if not self.skip_lcd:
+            self.lcd_sdk.update_text(message)
+
+    def detecting_plane(self, value: str) -> None:
+        """
+        Try to detect airplane base on value received from DCS-BIOS.
+
+        :param value: data from DCS-BIOS
+        """
+        short_name = value.replace('-', '').replace('_', '')
+        if self.plane_name != short_name:
+            self.plane_name = short_name
+            planes_list = get_planes_list(bios_dir=Path(str(get_config_yaml_item('dcsbios'))))
+            if self.plane_name in SUPPORTED_CRAFTS:
+                LOG.info(f'Advanced supported aircraft: {value}')
+                self.display = ['Detected aircraft:', SUPPORTED_CRAFTS[self.plane_name]['name']]
+                self.plane_detected = True
+            elif self.plane_name not in SUPPORTED_CRAFTS and value in planes_list:
+                LOG.info(f'Basic supported aircraft: {value}')
+                self.bios_name = value
+                self.display = ['Detected aircraft:', value]
+                self.plane_detected = True
+            elif value not in planes_list:
+                LOG.warning(f'Not supported aircraft: {value}')
+                self.display = ['Detected aircraft:', value, 'Not supported yet!']
+
+    def unload_old_plane(self):
+        """Unloads the previous plane by remove all callbacks and keep only one."""
+        LOG.debug(f'Unload start: {self.plane_name} Number of callbacks: {len(self.parser.write_callbacks)}')
+        for partial_obj in self.parser.write_callbacks:
+            for callback in partial_obj.func.__self__.callbacks:
+                if callback.func.__name__ == 'detecting_plane':
+                    self.parser.write_callbacks = {partial_obj}
+            if len(self.parser.write_callbacks) == 1:
+                break
+
+    def load_new_plane(self) -> None:
+        """
+        Dynamic load of new detected aircraft.
+
+        Setup callbacks for detected plane inside DCS-BIOS parser.
+        """
+        self.plane_detected = False
+        if self.plane_name in SUPPORTED_CRAFTS:
+            lcd_update_func = self.lcd_sdk.update_display if not self.skip_lcd else None
+            self.plane = getattr(import_module('dcspy.aircraft'), self.plane_name)(self.lcd, update_display=lcd_update_func)
+            LOG.debug(f'Dynamic load of: {self.plane_name} as AdvancedAircraft | BIOS: {self.plane.bios_name}')
+            self._setup_plane_callback()
+        else:
+            self.plane = MetaAircraft(self.plane_name, (BasicAircraft,), {})(self.lcd)
+            self.plane.bios_name = self.bios_name
+            LOG.debug(f'Dynamic load of: {self.plane_name} as BasicAircraft | BIOS: {self.plane.bios_name}')
+        LOG.debug(f'{repr(self)}')
+
+    def _setup_plane_callback(self):
+        """Setups DCS-BIOS parser callbacks for detected plane."""
+        plane_bios = get_full_bios_for_plane(plane=SUPPORTED_CRAFTS[self.plane_name]['bios'], bios_dir=Path(str(get_config_yaml_item('dcsbios'))))
+        for ctrl_name in self.plane.bios_data:
+            ctrl = plane_bios.get_ctrl(ctrl_name=ctrl_name)
+            dcsbios_buffer = getattr(dcsbios, ctrl.output.klass)
+            dcsbios_buffer(parser=self.parser, callback=partial(self.plane.set_bios, ctrl_name), **ctrl.output.args.model_dump())
+
     def gkey_callback_handler(self, key_idx: int, mode: int, key_down: int) -> None:
         """
         Logitech G-Key callback handler.
@@ -65,6 +158,33 @@ class LogitechDevice:
         LOG.debug(f'Button {gkey} is pressed, key down: {key_down}')
         self._send_request(button=gkey, key_down=key_down)
 
+    def check_buttons(self) -> LcdButton:
+        """
+        Check if button was pressed and return it`s enum.
+
+        :return: LcdButton enum of pressed button
+        """
+        for btn in self.buttons:
+            if self.lcd_sdk.logi_lcd_is_button_pressed(btn):
+                if not self.lcdbutton_pressed:
+                    self.lcdbutton_pressed = True
+                    return LcdButton(btn)
+                return LcdButton.NONE
+        self.lcdbutton_pressed = False
+        return LcdButton.NONE
+
+    def button_handle(self) -> None:
+        """
+        Button handler.
+
+        * detect if button was pressed
+        * sent action to DCS-BIOS via network socket
+        """
+        if not self.skip_lcd:
+            button = self.check_buttons()
+            if button.value:
+                self._send_request(button, key_down=KEY_DOWN)
+
     def _send_request(self, button: Union[LcdButton, Gkey], key_down: int) -> None:
         """
         Sent action to DCS-BIOS via network socket.
@@ -77,6 +197,29 @@ class LogitechDevice:
             LOG.debug(f'{button=}: {request=}')
             self.socket.sendto(request, SEND_ADDR)
             sleep(TIME_BETWEEN_REQUESTS)
+
+    def clear(self, true_clear=False) -> None:
+        """
+        Clear LCD.
+
+        :param true_clear:
+        """
+        LOG.debug(f'Clear LCD type: {self.lcd.type}')
+        self.lcd_sdk.clear_display(true_clear)
+
+    def _prepare_image(self) -> Image.Image:
+        """
+        Prepare image for base of LCD type.
+
+        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
+        For G19 takes first 8 or fewer elements of list and display as 8 rows.
+        :return: image instance ready display on LCD
+        """
+        img = Image.new(mode=self.lcd.mode.value, size=(self.lcd.width.value, self.lcd.height.value), color=self.lcd.background)
+        draw = ImageDraw.Draw(img)
+        for line_no, line in enumerate(self._display):
+            draw.text(xy=(0, self.vert_space * line_no), text=line, fill=self.lcd.foreground, font=self.lcd.font_s)
+        return img
 
     def __str__(self) -> str:
         return f'{type(self).__name__}: {self.lcd.width.value}x{self.lcd.height.value}'
@@ -188,98 +331,6 @@ class LcdKeyboard(LogitechDevice):
         self.lcd_sdk = lcd_sdk.LcdSdkManager(name='DCS World', lcd_type=self.lcd.type, skip=self.skip_lcd)
         self.vert_space = 0
 
-    @property
-    def display(self) -> List[str]:
-        """
-        Get the latest text from LCD.
-
-        :return: list of strings with data, row by row
-        """
-        return self._display
-
-    @display.setter
-    def display(self, message: List[str]) -> None:
-        """
-        Display message as image at LCD.
-
-        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
-        For G19 takes first 8 or fewer elements of list and display as 8 rows.
-        :param message: List of strings to display, row by row.
-        """
-        self._display = message
-        if not self.skip_lcd:
-            self.lcd_sdk.update_display(self._prepare_image())
-
-    def text(self, message: List[str]) -> None:
-        """
-        Display message at LCD.
-
-        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
-        For G19 takes first 8 or fewer elements of list and display as 8 rows.
-        :param message: List of strings to display, row by row.
-        """
-        if not self.skip_lcd:
-            self.lcd_sdk.update_text(message)
-
-    def detecting_plane(self, value: str) -> None:
-        """
-        Try to detect airplane base on value received from DCS-BIOS.
-
-        :param value: data from DCS-BIOS
-        """
-        short_name = value.replace('-', '').replace('_', '')
-        if self.plane_name != short_name:
-            self.plane_name = short_name
-            planes_list = get_planes_list(bios_dir=Path(str(get_config_yaml_item('dcsbios'))))
-            if self.plane_name in SUPPORTED_CRAFTS:
-                LOG.info(f'Advanced supported aircraft: {value}')
-                self.display = ['Detected aircraft:', SUPPORTED_CRAFTS[self.plane_name]['name']]
-                self.plane_detected = True
-            elif self.plane_name not in SUPPORTED_CRAFTS and value in planes_list:
-                LOG.info(f'Basic supported aircraft: {value}')
-                self.bios_name = value
-                self.display = ['Detected aircraft:', value]
-                self.plane_detected = True
-            elif value not in planes_list:
-                LOG.warning(f'Not supported aircraft: {value}')
-                self.display = ['Detected aircraft:', value, 'Not supported yet!']
-
-    def unload_old_plane(self):
-        """Unloads the previous plane by remove all callbacks and keep only one."""
-        LOG.debug(f'Unload start: {self.plane_name} Number of callbacks: {len(self.parser.write_callbacks)}')
-        for partial_obj in self.parser.write_callbacks:
-            for callback in partial_obj.func.__self__.callbacks:
-                if callback.func.__name__ == 'detecting_plane':
-                    self.parser.write_callbacks = {partial_obj}
-            if len(self.parser.write_callbacks) == 1:
-                break
-
-    def load_new_plane(self) -> None:
-        """
-        Dynamic load of new detected aircraft.
-
-        Setup callbacks for detected plane inside DCS-BIOS parser.
-        """
-        self.plane_detected = False
-        if self.plane_name in SUPPORTED_CRAFTS:
-            lcd_update_func = self.lcd_sdk.update_display if not self.skip_lcd else None
-            self.plane = getattr(import_module('dcspy.aircraft'), self.plane_name)(self.lcd, update_display=lcd_update_func)
-            LOG.debug(f'Dynamic load of: {self.plane_name} as AdvancedAircraft | BIOS: {self.plane.bios_name}')
-            self._setup_plane_callback()
-        else:
-            self.plane = MetaAircraft(self.plane_name, (BasicAircraft,), {})(self.lcd)
-            self.plane.bios_name = self.bios_name
-            LOG.debug(f'Dynamic load of: {self.plane_name} as BasicAircraft | BIOS: {self.plane.bios_name}')
-        LOG.debug(f'{repr(self)}')
-
-    def _setup_plane_callback(self):
-        """Setups DCS-BIOS parser callbacks for detected plane."""
-        plane_bios = get_full_bios_for_plane(plane=SUPPORTED_CRAFTS[self.plane_name]['bios'], bios_dir=Path(str(get_config_yaml_item('dcsbios'))))
-        for ctrl_name in self.plane.bios_data:
-            ctrl = plane_bios.get_ctrl(ctrl_name=ctrl_name)
-            dcsbios_buffer = getattr(dcsbios, ctrl.output.klass)
-            dcsbios_buffer(parser=self.parser, callback=partial(self.plane.set_bios, ctrl_name), **ctrl.output.args.model_dump())
-
     def check_buttons(self) -> LcdButton:
         """
         Check if button was pressed and return it`s enum.
@@ -306,29 +357,6 @@ class LcdKeyboard(LogitechDevice):
             button = self.check_buttons()
             if button.value:
                 self._send_request(button, key_down=KEY_DOWN)
-
-    def clear(self, true_clear=False) -> None:
-        """
-        Clear LCD.
-
-        :param true_clear:
-        """
-        LOG.debug(f'Clear LCD type: {self.lcd.type}')
-        self.lcd_sdk.clear_display(true_clear)
-
-    def _prepare_image(self) -> Image.Image:
-        """
-        Prepare image for base of LCD type.
-
-        For G13/G15/G510 takes first 4 or fewer elements of list and display as 4 rows.
-        For G19 takes first 8 or fewer elements of list and display as 8 rows.
-        :return: image instance ready display on LCD
-        """
-        img = Image.new(mode=self.lcd.mode.value, size=(self.lcd.width.value, self.lcd.height.value), color=self.lcd.background)
-        draw = ImageDraw.Draw(img)
-        for line_no, line in enumerate(self._display):
-            draw.text(xy=(0, self.vert_space * line_no), text=line, fill=self.lcd.foreground, font=self.lcd.font_s)
-        return img
 
 
 class G13(LcdKeyboard):
