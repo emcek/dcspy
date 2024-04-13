ef348019fdb2dcc6e5a5e2f82d235d0fbd0c003c
Michal Plichta <mplichta@gmail.com>
2024-04-13 14:02:41 +0200

    Refactor DLL path code into separate function

--- dcspy/sdk/__init__.py
+++ dcspy/sdk/__init__.py
@@ -37,17 +37,11 @@ def load_dll(lib_type: DllSdk) -> Optional[Union[Lib, CDLL]]:
     """
     Initialize and load of C dynamic linking library.
 
-    :param lib_type: library to load: LCD or LED
+    :param lib_type: library to load: LCD, LED or Gkey
     :return: C DLL instance
     """
     try:
-        arch = 'x64' if all([architecture()[0] == '64bit', maxsize > 2 ** 32, sizeof(c_void_p) > 4]) else 'x86'
-        try:
-            prog_files = environ['PROGRAMW6432']
-        except KeyError:
-            prog_files = environ['PROGRAMFILES']
-        dll_path = f'{prog_files}\\Logitech Gaming Software\\SDK\\{lib_type.dir}\\{arch}\\Logitech{lib_type.name.capitalize()}.dll'
-        LOG.debug(f'Selected DLL: {dll_path}')
+        dll_path = _get_ddl_path(lib_type)
 
         if lib_type.name == 'Gkey':
             dll = CDLL(dll_path)
@@ -63,3 +57,20 @@ def load_dll(lib_type: DllSdk) -> Optional[Union[Lib, CDLL]]:
         LOG.error(f'\n{header}\n*{type(err).__name__:^42}*\n{header}\nLoading of {lib_type.name} SDK failed !', exc_info=True)
         LOG.error(f'{header}')
         return None
+
+
+def _get_ddl_path(lib_type: DllSdk) -> str:
+    """
+    Return the path of the DLL file based on the provided library type.
+
+    :param lib_type: library to load: LCD, LED or Gkey
+    :return: The path of the DLL file as a string.
+    """
+    arch = 'x64' if all([architecture()[0] == '64bit', maxsize > 2 ** 32, sizeof(c_void_p) > 4]) else 'x86'
+    try:
+        prog_files = environ['PROGRAMW6432']
+    except KeyError:
+        prog_files = environ['PROGRAMFILES']
+    dll_path = f'{prog_files}\\Logitech Gaming Software\\SDK\\{lib_type.dir}\\{arch}\\Logitech{lib_type.name.capitalize()}.dll'
+    LOG.debug(f'Selected DLL: {dll_path}')
+    return dll_path
