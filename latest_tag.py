31e706ea1e2d95a19bf5ecfc0b831fc4408b62e7
Michal Plichta <mplichta@gmail.com>
2024-05-04 20:08:33 +0200

    Improve docstring in latest_tag.py

--- latest_tag.py
+++ latest_tag.py
@@ -7,11 +7,11 @@ from packaging import version
 
 def latest_version(repo_path: str, as_tag: int = 1) -> str:
     """
-    Get latest version number form repository.
+    Get the latest version number form repository.
 
-    :param repo_path: path to repository
+    :param repo_path: Path to repository
     :param as_tag: if True return full tag name
-    :return: return version or tag name as string
+    :return: Version or tag name as string
     """
     repo = git.Repo(repo_path)
     tags_list = [str(tag) for tag in repo.tags]
