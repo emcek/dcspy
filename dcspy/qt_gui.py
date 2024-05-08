54690d33e8823903e7901db8b40f25fc7915b3b4
Michal Plichta <mplichta@gmail.com>
2024-05-08 18:22:31 +0200

    Download and update CLI version when CLI version is running

--- dcspy/qt_gui.py
+++ dcspy/qt_gui.py
@@ -863,7 +863,7 @@ class DcsPyQtGui(QMainWindow):
         ver_string = get_version_string(repo=DCSPY_REPO_NAME, current_ver=__version__, check=True)
         self.statusbar.showMessage(ver_string)
         if 'update!' in ver_string:
-            self.systray.showMessage('DCSpy', f'New version: {__version__}', QIcon(':/icons/img/edit-download.svg'))
+            self.systray.showMessage('DCSpy', f'New: {ver_string}', QIcon(':/icons/img/edit-download.svg'))
             self._download_new_release()
         elif 'latest' in ver_string:
             self._show_message_box(kind_of=MsgBoxTypes.INFO, title='No updates', message='You are running latest version')
@@ -879,7 +879,8 @@ class DcsPyQtGui(QMainWindow):
 
     def _restart_pyinstaller_ver(self):
         """Download and restart a new version of DCSpy when using an executable/pyinstaller version."""
-        ext = '.exe' if 'cli' not in Path(sys.executable).name else '_cli.exe'
+        cli = '' if 'cli' not in Path(sys.executable).name else '_cli'
+        ext = f'{cli}.exe'
         rel_info = check_ver_at_github(repo='emcek/dcspy', current_ver=__version__, extension=ext)
         exe_parent_dir = Path(sys.executable).parent
         reply = self._show_message_box(kind_of=MsgBoxTypes.QUESTION, title='Update DCSpy',
@@ -888,8 +889,8 @@ class DcsPyQtGui(QMainWindow):
         if bool(reply == QMessageBox.StandardButton.Yes):
             try:
                 destination = exe_parent_dir / rel_info.asset_file
-                old_ver_dst = exe_parent_dir / f'dcspy_{__version__}.exe'
-                new_ver_dst = exe_parent_dir / 'dcspy.exe'
+                old_ver_dst = exe_parent_dir / f'dcspy{cli}_{__version__}.exe'
+                new_ver_dst = exe_parent_dir / f'dcspy{cli}.exe'
                 os.rename(src=Path(sys.executable), dst=old_ver_dst)
                 LOG.debug(f'Rename: {Path(sys.executable)} -> {old_ver_dst}')
                 download_file(url=rel_info.dl_url, save_path=destination)
