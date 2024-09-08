065458aeec1d70a728c8aeefeeddd296b94138b7
Michal Plichta <mplichta@gmail.com>
2024-09-08 19:57:46 +0200

    double-quote-string-fixer

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -5,29 +5,29 @@ import mkdocs_gen_files  # type: ignore
 nav = mkdocs_gen_files.Nav()
 
 root = Path(__file__).parent.parent
-src = root / "src"
+src = root / 'src'
 
-for path in sorted(src.rglob("*.py")):
-    module_path = path.relative_to(src).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("reference", doc_path)
+for path in sorted(src.rglob('*.py')):
+    module_path = path.relative_to(src).with_suffix('')
+    doc_path = path.relative_to(src).with_suffix('.md')
+    full_doc_path = Path('reference', doc_path)
 
     parts = tuple(module_path.parts)
 
-    if parts[-1] == "__init__":
+    if parts[-1] == '__init__':
         parts = parts[:-1]
-        doc_path = doc_path.with_name("index.md")
-        full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
+        doc_path = doc_path.with_name('index.md')
+        full_doc_path = full_doc_path.with_name('index.md')
+    elif parts[-1] == '__main__':
         continue
 
     nav[parts] = doc_path.as_posix()
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(parts)
+    with mkdocs_gen_files.open(full_doc_path, 'w') as fd:
+        ident = '.'.join(parts)
         fd.write(f"::: {ident}")
 
     mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open('reference/SUMMARY.md', 'w') as nav_file:
     nav_file.writelines(nav.build_literate_nav())
