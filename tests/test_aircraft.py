ac241b119d036a5921dd95f23242fe68b9cd658a
Michal Plichta <mplichta@gmail.com>
2024-12-16 14:33:55 +0100

    fetch 2022Server test pictures

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -333,10 +333,12 @@ def test_prepare_image_for_all_planes(model, lcd, resources, img_precision, requ
     set_bios_during_test(aircraft_model, bios_pairs)
     img = aircraft_model.prepare_image()
     ref_file_base_path = resources / platform / uname().release if platform == 'win32' else resources / platform
-    # if 'f4e' in model:
-    #     img.save(ref_file_base_path / f'new_{model}_{lcd}_{type(aircraft_model).__name__}.png')
-    # else:
-    assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_{lcd}_{type(aircraft_model).__name__}.png', precision=img_precision)
+    from os import mkdir
+    mkdir(path=ref_file_base_path, exist_ok=True)
+    if uname().release == '2022Server':
+        img.save(ref_file_base_path / f'{model}_{lcd}_{type(aircraft_model).__name__}.png')
+    else:
+        assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_{lcd}_{type(aircraft_model).__name__}.png', precision=img_precision)
 
 
 @mark.benchmark
@@ -362,7 +364,12 @@ def test_prepare_image_for_apache_wca_mode(model, resources, img_precision, requ
     img = apache.prepare_image()
     assert (Path(gettempdir()) / f'{type(apache).__name__}_999.png').exists()
     ref_file_base_path = resources / platform / uname().release if platform == 'win32' else resources / platform
-    assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode.png', precision=img_precision)
+    from os import mkdir
+    mkdir(path=ref_file_base_path, exist_ok=True)
+    if uname().release == '2022Server':
+        img.save(ref_file_base_path / f'{model}_wca_mode.png')
+    else:
+        assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode.png', precision=img_precision)
 
 
 # <=><=><=><=><=> Apache special <=><=><=><=><=>
@@ -386,7 +393,12 @@ def test_apache_wca_more_then_one_screen_scrolled(model, resources, img_precisio
     assert apache.warning_line == 3
     img = apache.prepare_image()
     ref_file_base_path = resources / platform / uname().release if platform == 'win32' else resources / platform
-    assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode_scroll_3.png', precision=img_precision)
+    from os import mkdir
+    mkdir(path=ref_file_base_path, exist_ok=True)
+    if uname().release == '2022Server':
+        img.save(ref_file_base_path / f'{model}_wca_mode_scroll_3.png')
+    else:
+        assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode_scroll_3.png', precision=img_precision)
 
     for i in range(1, 3):
         apache.warning_line += 1
@@ -395,7 +407,12 @@ def test_apache_wca_more_then_one_screen_scrolled(model, resources, img_precisio
     img = apache.prepare_image()
     assert apache.warning_line == 1
     ref_file_base_path = resources / platform / uname().release if platform == 'win32' else resources / platform
-    assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode_scroll_1.png', precision=img_precision)
+    from os import mkdir
+    mkdir(path=ref_file_base_path, exist_ok=True)
+    if uname().release == '2022Server':
+        img.save(ref_file_base_path / f'{model}_wca_mode_scroll_1.png')
+    else:
+        assert compare_images(img=img, file_path=ref_file_base_path / f'{model}_wca_mode_scroll_1.png', precision=img_precision)
 
 
 @mark.benchmark
