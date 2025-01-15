36cf9c662f8cd9ff7ee0f2dee171c2e6915918ba
Michal Plichta <mplichta@gmail.com>
2025-01-15 21:27:22 +0100

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -265,12 +265,12 @@ def default_config():
     """Get default configuration dict."""
     from os import environ
     return {
-        'dcsbios': f'C:\\Users\\{environ.get("USERNAME", "UNKNOWN")}\\Saved Games\\DCS\\Scripts\\DCS-BIOS',
-        'dcs': 'C:\\Program Files\\Eagle Dynamics\\DCS World', 'device': 'G13', 'save_lcd': False, 'show_gui': True, 'autostart': False,
+        'dcsbios': f'C:/Users/{environ.get("USERNAME", "UNKNOWN")}/Saved Games/DCS/Scripts/DCS-BIOS',
+        'dcs': 'C:/Program Files/Eagle Dynamics/DCS World', 'device': 'G13', 'save_lcd': False, 'show_gui': True, 'autostart': False,
         'verbose': False, 'check_bios': True, 'check_ver': True, 'font_name': models.DEFAULT_FONT_NAME, 'font_mono_m': 11, 'font_mono_s': 9, 'font_mono_l': 16,
         'font_color_m': 22, 'font_color_s': 18, 'font_color_l': 32, 'f16_ded_font': True, 'git_bios': True, 'git_bios_ref': 'master', 'toolbar_style': 0,
         'toolbar_area': 4, 'gkeys_area': 2, 'gkeys_float': False, 'theme_mode': 'system', 'theme_color': 'dark-blue', 'completer_items': 20,
-        'current_plane': 'A-10A',
+        'current_plane': 'A-10C', 'api_ver': '3.6.1'
     }
 
 
