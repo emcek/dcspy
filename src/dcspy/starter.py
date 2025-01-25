0d9e9abfbf7fa26808cda460aa662a63acbf1d95
Michal Plichta <mplichta@gmail.com>
2025-01-25 15:43:17 +0100

    use text mode in starter and add global state for cleaning (before and after)

--- src/dcspy/starter.py
+++ src/dcspy/starter.py
@@ -13,7 +13,8 @@ from dcspy.models import DCSPY_REPO_NAME, MULTICAST_IP, RECV_ADDR, Color, Logite
 from dcspy.utils import check_bios_ver, get_version_string
 
 LOG = getLogger(__name__)
-LOOP_FLAG = True
+CLEAN_BEFORE_LOAD_PLANE = False
+CLEAN_WHILE_WAIT_FOR_DATA = False
 SUPPORTERS = ['Jon Wardell', 'Simon Leigh', 'Alexander Leschanz', 'Sireyn', 'Nick Thain', 'BrotherBloat']
 __version__ = '3.6.3'
 
@@ -29,11 +30,18 @@ def _handle_connection(logi_device: LogitechDevice, parser: ProtocolParser, sock
     :param event: Stop event for the main loop
     """
     start_time = time()
+    global CLEAN_BEFORE_LOAD_PLANE
+    global CLEAN_WHILE_WAIT_FOR_DATA
     LOG.info('Waiting for DCS connection...')
     support_banner = _supporters(text=f'Huge thanks to: {", ".join(SUPPORTERS)} and others! For support and help! ', width=37)
     while not event.is_set():
         try:
             dcs_bios_resp = sock.recv(2048)
+            if CLEAN_BEFORE_LOAD_PLANE:
+                LOG.debug(f'Before: {CLEAN_WHILE_WAIT_FOR_DATA=} {CLEAN_BEFORE_LOAD_PLANE=}')
+                logi_device.clear(true_clear=True)
+                CLEAN_BEFORE_LOAD_PLANE = False
+                CLEAN_WHILE_WAIT_FOR_DATA = True
             for int_byte in dcs_bios_resp:
                 parser.process_byte(int_byte)
             start_time = time()
@@ -49,11 +57,11 @@ def _load_new_plane_if_detected(logi_device: LogitechDevice) -> None:
 
     :param logi_device: Type of Logitech keyboard with LCD
     """
-    global LOOP_FLAG
+    global CLEAN_WHILE_WAIT_FOR_DATA
     if logi_device.plane_detected:
         logi_device.unload_old_plane()
         logi_device.load_new_plane()
-        LOOP_FLAG = True
+        CLEAN_WHILE_WAIT_FOR_DATA = True
 
 
 def _supporters(text: str, width: int) -> Iterator[str]:
@@ -79,16 +87,20 @@ def _sock_err_handler(logi_device: LogitechDevice, start_time: float, ver_string
     :param support_iter: Iterator for banner supporters
     :param exp: Caught exception instance
     """
-    global LOOP_FLAG
-    if LOOP_FLAG:
+    global CLEAN_WHILE_WAIT_FOR_DATA
+    global CLEAN_BEFORE_LOAD_PLANE
+    if CLEAN_WHILE_WAIT_FOR_DATA:
         LOG.debug(f'Main loop socket error: {exp}')
-        LOOP_FLAG = False
+        LOG.debug(f'After clean: {CLEAN_WHILE_WAIT_FOR_DATA=} {CLEAN_BEFORE_LOAD_PLANE=}')
+        logi_device.clear(true_clear=True)
+        CLEAN_BEFORE_LOAD_PLANE = True
+        CLEAN_WHILE_WAIT_FOR_DATA = False
     wait_time = gmtime(time() - start_time)
-    logi_device.display = [('     DCSpy       ', Color.orange),
-                           ('Logitech LCD OK', Color.lightgreen),
-                           (f'No data from DCS:    {wait_time.tm_min:02d}:{wait_time.tm_sec:02d}', Color.red),
-                           (f'{next(support_iter)}', Color.yellow),
-                           (ver_string, Color.white)]
+    logi_device.text = [('     DCSpy       ', Color.orange),
+                        ('Logitech LCD OK', Color.lightgreen),
+                        (f'No data from DCS:      {wait_time.tm_min:02d}:{wait_time.tm_sec:02d}', Color.red),
+                        (f'{next(support_iter)}', Color.yellow),
+                        (ver_string, Color.white)]
 
 
 def _prepare_socket() -> socket.socket:
@@ -121,8 +133,8 @@ def dcspy_run(model: LogitechDeviceModel, event: Event) -> None:
         dcspy_ver = get_version_string(repo=DCSPY_REPO_NAME, current_ver=__version__, check=bool(get_config_yaml_item('check_ver')))
         _handle_connection(logi_device=logi_dev, parser=parser, sock=dcs_sock, ver_string=dcspy_ver, event=event)
     LOG.info('DCSpy stopped.')
-    logi_dev.display = [('     DCSpy       ', Color.orange),
-                        ('DCSpy stopped', Color.red),
-                        ('', Color.black),
-                        (f'DCSpy:    {dcspy_ver}', Color.white),
-                        (f'DCS-BIOS:  {check_bios_ver(bios_path=get_config_yaml_item("dcsbios"))}', Color.white)]
+    logi_dev.text = [('     DCSpy       ', Color.orange),
+                     ('DCSpy stopped', Color.red),
+                     ('', Color.black),
+                     (f'DCSpy:    {dcspy_ver}', Color.white),
+                     (f'DCS-BIOS:  {check_bios_ver(bios_path=get_config_yaml_item("dcsbios"))}', Color.white)]
