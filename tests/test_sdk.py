c93c981845039cb4c677615997391726be59f538
Michal Plichta <mplichta@gmail.com>
2025-06-18 13:48:17 +0200

    fix isort in tests

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -1,5 +1,5 @@
 def test_load_dll():
-    from dcspy.sdk import load_dll, DllSdk
+    from dcspy.sdk import DllSdk, load_dll
 
     test_dll = DllSdk(name='TEST', dir='TEST', header='test_header')
 
