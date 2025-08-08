a3effd7d57779c92e0ead4726d9c908d5dc6cfcc
Michal Plichta <mplichta@gmail.com>
2025-08-08 20:16:13 +0200

    fir extra/test script led

--- src/dcspy/led.py
+++ src/dcspy/led.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from dcspy.models import LOGI_DEVICETYPE_ALL, LOGI_LED_DURATION_INFINITE
+from dcspy.models import LedConstants
 from dcspy.sdk import led_sdk
 
 rgb = 100, 0, 0
@@ -9,20 +9,23 @@ interval = 270
 
 
 def flash():
-    led_sdk.logi_led_flash_lighting(rgb, LOGI_LED_DURATION_INFINITE, interval)
+    """Flash the LED with the specified RGB color, duration, and interval."""
+    led_sdk.logi_led_flash_lighting(rgb, LedConstants.LOGI_LED_DURATION_INFINITE.value, interval)
 
 
 def pulse():
-    led_sdk.logi_led_pulse_lighting(rgb, LOGI_LED_DURATION_INFINITE, interval)
+    """Pulse the LED with the specified RGB color, duration, and interval."""
+    led_sdk.logi_led_pulse_lighting(rgb, LedConstants.LOGI_LED_DURATION_INFINITE.value, interval)
 
 
 def stop():
+    """Stop any of the preset effect."""
     led_sdk.logi_led_stop_effects()
 
 
 led_sdk.logi_led_init()
 sleep(0.05)
-led_sdk.logi_led_set_target_device(LOGI_DEVICETYPE_ALL)
+led_sdk.logi_led_set_target_device(LedConstants.LOGI_DEVICETYPE_ALL)
 sleep_time = duration + 0.2
 # led_sdk.logi_led_flash_lighting(rgb, duration * 1000, interval)
 for _ in range(20):
