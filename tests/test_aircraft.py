6422b3ceec348719f8627733f7f5de46e3d97846
Michal Plichta <mplichta@gmail.com>
2025-08-24 23:01:43 +0200

    update test_prepare_image_for_viper_color_non_ded to add assert

--- tests/test_aircraft.py
+++ tests/test_aircraft.py
@@ -345,8 +345,8 @@ def test_prepare_image_for_viper_color_non_ded(resources, img_precision, request
     set_bios_during_test(aircraft_model, bios_pairs)
     img = aircraft_model.prepare_image()
     ref_file_base_path = resources / platform / uname().release if platform == 'win32' else resources / platform
-    img.save(ref_file_base_path / f'{ref_file_base_path}_f16c50_color_non_ded_{type(aircraft_model).__name__}.png')
-    # assert compare_images(img=img, file_path=ref_file_base_path / f'f16c50_color_non_ded_{type(aircraft_model).__name__}.png', precision=img_precision)
+    # img.save(ref_file_base_path / f'{ref_file_base_path}_f16c50_color_non_ded_{type(aircraft_model).__name__}.png')
+    assert compare_images(img=img, file_path=ref_file_base_path / f'f16c50_color_non_ded_{type(aircraft_model).__name__}.png', precision=img_precision)
 
 
 @mark.benchmark
