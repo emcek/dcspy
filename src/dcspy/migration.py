1a156823caf05b071bd71f6f1929450d053266e1
Michal Plichta <mplichta@gmail.com>
2026-01-23 21:08:55 +0100

    add 3.8.0 migration policy

--- src/dcspy/migration.py
+++ src/dcspy/migration.py
@@ -10,7 +10,7 @@ from shutil import SameFileError, copy
 
 from packaging import version
 
-from dcspy.models import ConfigValue, DcspyConfigYaml, __version__
+from dcspy.models import BIOS_REPO_ADDR, ConfigValue, DcspyConfigYaml, __version__
 from dcspy.utils import DEFAULT_YAML_FILE, defaults_cfg, get_config_yaml_location
 
 LOG = getLogger(__name__)
@@ -54,6 +54,15 @@ def _filter_api_ver_func(cfg_ver: str) -> Iterator[Callable[[DcspyConfigYaml], N
     for api_ver in api_ver_list:
         if version.Version(api_ver) > version.Version(cfg_ver) <= version.Version(__version__):
             yield globals()['_api_ver_{}'.format(api_ver.replace('.', '_'))]
+
+
+def _api_ver_3_8_0(cfg: DcspyConfigYaml) -> None:
+    """
+    Migrate to version 3.8.0.
+
+    :param cfg: Configuration dictionary
+    """
+    _add_key(cfg, 'git_bios_repo', BIOS_REPO_ADDR)
 
 
 def _api_ver_3_7_0(cfg: DcspyConfigYaml) -> None:
