cb808029b60a34bfeedc3e6e9ad65d81c966343d
Michal Plichta <mplichta@gmail.com>
2026-03-24 16:15:29 +0100

    secure gen_hashes.py

--- scripts/gen_hashes.py
+++ scripts/gen_hashes.py
@@ -6,15 +6,19 @@ ALGORITHMS = ['sha3_224', 'sha3_512', 'sha3_384', 'sha1', 'sha512', 'sha384',
               'blake2b', 'sha3_256', 'sha256', 'md5', 'sha224', 'blake2s']
 
 
-def generate(files: list[Path], output: Path, algorithms:list[str]) -> None:
+def generate(files: list[Path], output_file: str, algorithms:list[str]) -> None:
     """
     Generate hashes for list of files.
 
     :param files: List of Path objects
-    :param output: Path to output file
+    :param output_file: output file
     :param algorithms: List of hash algorithms to use
     """
-    with open(output, 'w+') as f_digests:
+    wd = Path(__file__).resolve().parent.parent
+    fd = Path(output_file).resolve().parent
+    if wd != fd:
+        raise ValueError(f'Output file {output_file} must be in the same directory as the script')
+    with open(output_file, 'w+') as f_digests:
         for algo in algorithms:
             f_digests.write(f'#HASH {algo}\n')
             for file2hash in files:
@@ -27,7 +31,7 @@ def generate(files: list[Path], output: Path, algorithms:list[str]) -> None:
 if __name__ == '__main__':
     parser = ArgumentParser(description='Generate hashes for files.')
     parser.add_argument('files', nargs='+', type=Path, help='List of files to hash')
-    parser.add_argument('-o', '--output', type=Path, default=Path('./DIGESTS'), help='Output file for digests')
+    parser.add_argument('-o', '--output', type=str, default='DIGESTS', help='Output file for digests')
     parser.add_argument('-a', '--algorithms', nargs='+', default=ALGORITHMS, choices=ALGORITHMS,
                         help='List of hash algorithms to use')
     args = parser.parse_args()
