a92cd53d7ce247b1321a4362675f0e7780173821
Michal Plichta <mplichta@gmail.com>
2026-03-24 14:14:24 +0100

    adjust log formatter to improve alignment consistency

--- src/dcspy/log.py
+++ src/dcspy/log.py
@@ -14,14 +14,14 @@ def config_logger(logger: Logger, verbose: bool = False) -> None:
     """
     logger.setLevel(DEBUG)
     file_hand = RotatingFileHandler(filename=Path(gettempdir()) / 'dcspy.log', mode='a', encoding='utf-8', maxBytes=5 * 1024 * 1024, backupCount=1)
-    file_hand.setFormatter(Formatter('%(asctime)s | %(name)-17s | %(levelname)-7s | %(threadName)-10s | %(message)s / %(funcName)s:%(lineno)d'))
+    file_hand.setFormatter(Formatter(fmt='%(asctime)s | %(name)-17s | %(levelname)-8s | %(threadName)-10s | %(message)s / %(funcName)s:%(lineno)d'))
     file_hand.setLevel(INFO)
     stream_hand = StreamHandler()
     stream_hand.setLevel(INFO)
     if verbose:
         file_hand.setLevel(DEBUG)
         stream_hand.setLevel(DEBUG)
-    stream_hand.setFormatter(Formatter('%(levelname)-7s | %(message)s'))
+    stream_hand.setFormatter(Formatter(fmt='%(levelname)-8s | %(message)s'))
     logger.addHandler(stream_hand)
     logger.addHandler(file_hand)
     header = '#' * 60
